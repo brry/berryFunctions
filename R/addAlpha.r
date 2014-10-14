201f0364888f56d35ac9bbea5aa6f82c7cb85e19
BerryBoessenkool <berryboessenkool@hotmail.com>
2014-10-14 20:40:28 +0200

    addAlpha: correct output if length(col)==1

--- R/addAlpha.r
+++ R/addAlpha.r
@@ -5,14 +5,12 @@
 
 addAlpha <- function(
   col, # vector of color names (\code{\link{colors}) that can be interpreted by \code{\link{col2rgb}}
-  alpha=1 # Level of semi-transparency. between 0 (transparent) and 1 (intransparent).
+  alpha=0.3 # Level of semi-transparency. between 0 (transparent) and 1 (intransparent).
   )
 {
-output <- apply(X=sapply(col,col2rgb)/255,
-                MARGIN=2,
-                FUN=function(x) rgb(x[1], x[2], x[3], alpha=alpha))
-if( length(alpha) == 1) return(output) else
+rgb2 <-  function(x) rgb(x[1], x[2], x[3], alpha=alpha)
+output <- apply(X=sapply(col,col2rgb)/255, MARGIN=2, FUN=rgb2)
+if( length(alpha)==1 | length(col)==1)  return(as.vector(output)) else
 if( length(alpha) == length(col) ) return(diag(output)) else
 warning("col and alpha had different lengths"); return(output)
 }
-
