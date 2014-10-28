6b3133218ad64887134dcc91d95afc123e8c1255
BerryBoessenkool <berryboessenkool@hotmail.com>
2014-10-28 08:45:10 +0100

    linReg now takes formula

--- R/linReg.R
+++ R/linReg.R
@@ -1,6 +1,7 @@
 linReg <- function(
    x,
-   y,
+   y=NULL,
+   data=NULL,
    digits=2,
    inset=0,
    xpd=par("xpd"),
@@ -12,10 +13,27 @@ linReg <- function(
    lwd=1,
    xlab=deparse(substitute(x)),
    ylab=deparse(substitute(y)),
+   main="linear regression",
    ...)
 {
+if(class(x)=="formula")
+  {
+  if(!missing(data))
+    {                   # get x and y from data.frame
+    name <- as.character(x)[-1]
+    x <- data[ , name[2] ]  ;  if(missing(xlab)) xlab <- name[2]
+    y <- data[ , name[1] ]  ;  if(missing(ylab)) ylab <- name[1]
+    if(missing(main)) main <- paste("linear regression of",
+                                    deparse(substitute(data)))
+    } else
+    {                   # get x and y from formula directly
+    name <- as.character(x)[-1]
+    x <- get(name[2]) ;  if(missing(xlab)) xlab <- name[2]
+    y <- get(name[1]) ;  if(missing(ylab)) ylab <- name[1]
+    }
+  }
 # make new plot if add is FALSE (the default):
-if (!add) plot(x, y, las=1, pch=pch, xlab=xlab, ylab=ylab, ...)
+if (!add) plot(x, y, las=1, pch=pch, xlab=xlab, ylab=ylab, main=main, ...)
 # do linear regression and plotting
 mod <- lm( y ~ x )
 abline(mod, col=col, lwd=lwd, ...)
@@ -27,7 +45,7 @@ if(is.na(digits[4])) digits[4] <- digits[1]
 a <- round( coef(mod)[2] , digits[1])
 b <- round( coef(mod)[1] , digits[2])
 r <- round( summary(mod)$r.squared , digits[3])
-Txt <- paste0("y = ", a, " * x ", ifelse(b>0," + ", " - "), abs(b), "\nR^2 = ",
+Txt <- paste0("y = ", a, " * x ", ifelse(b>0," + ", " - "), abs(b), "\nR\U00B2 = ",
           r, "\nRMSE = ", signif(rmse(x,y), digits[4]))
 # write formula
 legend(pos1, pos2, Txt, bty="n", text.col=col, inset=inset, xpd=xpd)
