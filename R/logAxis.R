98169f08f84521b01158a94977654f3735db159e
BerryBoessenkool <berryboessenkool@hotmail.com>
2014-12-05 03:18:18 +0100

    logAxis: good internal range and base defaults. now enables exponents. side can be a vector.

--- R/logAxis.R
+++ R/logAxis.R
@@ -1,44 +1,67 @@
 # Function to label logarithmic axes
-# berry-b@gmx.de, Feb 2014, idea 2013
+# berry-b@gmx.de, Sept 2014
 
 logAxis <- function(
-  side=1,      # Which \code{\link{axis}} is to be labeled?
+  side=1,      # Which \code{\link{axis}} are to be labeled?
   log=NULL,    # Is the axis logarithmic by plot(log="x")? internal DEFAULT: par("xlog") or "ylog"
   lcol="grey", # Color of gridlines drawn in the graph with \code{\link{abline}}, NA to suppress.
   lty=1, lwd=1,# Type of gridlines
   expr,        # Expression drawing over the ablines, like (points(x,y). Can be code within {braces}.
   las=1,       # LabelAxisStyle for the orientation of the labels
-  from=-7,               # Lower exponent OR vector with data, as in \code{\link{logVals}}
-  to=7,                  # High end exponent
-  Range=range(from, to), # Override from and to as range
-  base=c(1,2,5),         # Bases to be used, eg. c(1,2,5) or 1
-  big.mark="'",          # Symbol separating thousands, eg. space, comma, dot, etc. see "format" and "prettyNum"
-  decimal.mark=".",      # Character separating comma values, see "format" and "prettyNum"
-  scientific=FALSE,      # See \code{\link{format}}
-  ...)         # further arguments passed to axis, like \code{lwd, col.ticks, hadj, lty}, ...
+  from,             # Lower exponent OR vector with data, as in \code{\link{logVals}}
+  to,               # High end exponent
+  Range,            # Override from and to as range
+  base,             # Bases to be used, eg. c(1,2,5) or 1
+  big.mark="'",     # Symbol separating thousands, eg. space, comma, dot, etc. see "format" and "prettyNum"
+  decimal.mark=".", # Character separating comma values, see "format" and "prettyNum"
+  scientific=FALSE, # See \code{\link{format}}
+  exponent=5,       # Starting at which exponent should \code{\link{logVals}} return an expression with exponents? DEFAULT: 5
+  expobase1=FALSE,  # Should "n * " be appended before 10^exp if n=1?
+  allbase=1:9,      # base for \code{$all} (for horizontal lines)
+    ...)            # further arguments passed to axis, like \code{lwd, col.ticks, hadj, lty}, ...
 {
-# get labels and positions:
-lv <- logVals(from=from, to=to, Range=Range, base=base, big.mark=big.mark,
-              decimal.mark=decimal.mark, scientific=scientific)
-# choose vertical or horizontal lines:
-if(side==1 | side==3)
+for(side_i in side)
+{ # loop around each side
+if(side_i==1 | side_i==3) # vertical lines, labels at x-axis:
   {
+  # set from and to:
+  from_i <- if(missing(from)) par("usr")[1] else from
+  to_i   <- if(missing(to))   par("usr")[2] else to
+  # determine base:
+  base_i <- if(missing(base)) {if(abs(to_i-from_i)>4) 1 else c(1,2,5)} else base
+  # get labels and positions:
+  lv <- logVals(from=from_i, to=to_i, Range=Range, base=base_i, big.mark=big.mark,
+            decimal.mark=decimal.mark, scientific=scientific, exponent=exponent,
+            expobase1=expobase1, allbase=allbase)
+  # draw lines
   if(is.null(log)) log <- par("xlog")
   if(log) abline(v=lv$all,     col=lcol, lty=lty, lwd=lwd)
   else abline(v=log10(lv$all), col=lcol, lty=lty, lwd=lwd)
   }
-else
+else # horizontal lines, labels at y-axis:
   {
+  # set from and to:
+  from_i <- if(missing(from)) par("usr")[3] else from
+  to_i   <- if(missing(to))   par("usr")[4] else to
+  # determine base:
+  base_i <- if(missing(base)) {if(abs(to_i-from_i)>4) 1 else c(1,2,5)} else base
+  # get labels and positions:
+  lv <- logVals(from=from_i, to=to_i, Range=Range, base=base_i, big.mark=big.mark,
+            decimal.mark=decimal.mark, scientific=scientific, exponent=exponent,
+            expobase1=expobase1, allbase=allbase)
+  # draw lines
   if(is.null(log)) log <- par("ylog")
   if(log) abline(h=lv$all,     col=lcol, lty=lty, lwd=lwd)
   else abline(h=log10(lv$all), col=lcol, lty=lty, lwd=lwd)
   }
-box("plot")
 # axis labels:
-if(log) axis(side=side, at=lv$vals,        labels=lv$labs, las=las, ...)
-else    axis(side=side, at=log10(lv$vals), labels=lv$labs, las=las, ...)
+if(log) axis(side=side_i, at=lv$vals,        labels=lv$labs, las=las, ...)
+else    axis(side=side_i, at=log10(lv$vals), labels=lv$labs, las=las, ...)
+} # End of loop
+# Box to cover up the lines plotted over the existing box:
+box("plot")
 # overplot ablines with expr:
 if(!missing(expr)) expr
 # output:
 return(invisible(lv))
-}
+} # End of function
--- R/logVals.R
+++ R/logVals.R
@@ -4,11 +4,15 @@
 logVals <- function(
   from=-7,               # lower exponent OR vector with data
   to=7,                  # high end
-  Range=range(from, to), # or give from and to as range
+  Range,                 # or give from and to as range
   base=1,                # bases to be used, eg. c(1,2,5)
   big.mark="'",          # symbol separating thousands, eg. space, comma, dot, etc. see "format" and "prettyNum"
   decimal.mark=".",      # character separating comma values, see "format" and "prettyNum"
-  scientific=FALSE)      # see "format"
+  scientific=FALSE,      # see "format"
+  exponent=Inf,          # like scipen, but for expression (to be used in logAxis)
+  expobase1=FALSE,       # Should "n * " be appended before 10^exp if n=1?
+  allbase=1:9            # base for \code{$all} (for horizontal lines)
+  )
 {
 # Calculate the exponents from vector, if given as first argument:
 if( missing(to)  &  NROW(from)>1  )
@@ -24,12 +28,32 @@ if( !missing(Range)  )
   to <- ceiling(Range[2])
   }
 # values for lines and labels:
-vals <- base*10^rep(from:to, each=length(base))
+vals <- base*10^rep(floor(from):ceiling(to), each=length(base))
 # formatted values for labels:
 labs <-  format(vals, big.mark=big.mark, trim=TRUE, scientific=scientific, 
                       drop0trailing=TRUE, decimal.mark=decimal.mark)
+# change to expression if value > exponent :
+change1 <- abs(log10(vals)) >= exponent  & log10(vals)%%1 ==0 # base=1
+change2 <- abs(log10(vals)) >= exponent  & log10(vals)%%1 !=0 # other base
+if(expobase1)
+  {
+  change2 <- change1 | change2        # all should be treated as other bases
+  change1 <- rep(FALSE, length(vals))
+  }
+if(any(change1 | change2)) # only deal with expression if applicable:
+  {
+  w2c1 <- which(change1)  # w2c= which to change
+  w2c2 <- which(change2)
+  wnc <- which(!change1 & !change2)
+  labs2 <- vector("expression", length(labs))
+  for(i in w2c1) labs2[[i]] <- bquote(10^.(floor(log10(vals))[i]))
+  for(i in w2c2) labs2[[i]] <- bquote(
+         .(rep(base, length.out=length(vals))[i])%.%10^.(floor(log10(vals))[i]))
+  for(i in wnc) labs2[[i]] <- labs[i]  # regular labels for all entries not
+  labs <- labs2                        # affected by the exponent size rule
+  }
 # Values for lines:
-all <- 1:9 * 10^rep(from:to, each=9)
+all <- allbase * 10^rep(floor(from):ceiling(to), each=length(allbase))
 # return end result
 list(vals=vals, labs=labs, all=all)
 }
