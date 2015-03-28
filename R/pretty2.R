4fbf81a53df33a811c1ee5e63aaeed54bcca9038
BerryBoessenkool <berryboessenkool@hotmail.com>
2015-03-29 00:55:48 +0100

    pretty2: force argument added

--- R/pretty2.R
+++ R/pretty2.R
@@ -1,10 +1,18 @@
 # Berry Boessenkool, Aug 2014
 # pretty with no values outside of x range
-pretty2 <- function(x, ...)
+pretty2 <- function(x, n=5, force=FALSE, ...)
 {
-p <- pretty(x, ...)
+p <- pretty(x, n=n, ...)
 r <- range(x, finite=TRUE)
 p <- p[p>=r[1] & p<=r[2]]
+if(force)                   # Added March 2015
+  while(length(p) != n)
+  {
+  x <- extendrange(x, f=-0.01)
+  p <- pretty(x, n=n, ...)
+  p <- p[p>=r[1] & p<=r[2]]
+  if(length(p) > n) p <- range(p)
+  }
 p
 }
 
