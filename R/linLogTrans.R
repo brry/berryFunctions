93918b34e6c57e885d6f4ac2234f6fff225be0c5
brry <berryboessenkool@hotmail.com>
2015-04-29 11:01:02 +0200

    linLogTrans now uses continuous transformation t values. xlim, ylim arguments added. Documentation greatly expanded, especially examples.

--- R/linLogTrans.R
+++ R/linLogTrans.R
@@ -9,6 +9,8 @@ linLogTrans <- function(
    base=1, # base passed to \code{\link{logVals}}
    las=1, # \code{\link{par}} LabelAxisStyle (numbers upright)
    plot=TRUE, # Plot animations at all? False to just get the t-vector (used in \code{\link{linLogHist}})
+   xlim=range(x, finite=TRUE), # xlim range in non-log units.
+   ylim=range(y, finite=TRUE), # ylim range in non-log units.
    firstplot=TRUE, # plot on linear scale first?
    lastplot=TRUE, # plot on logarithmic scale at the end?
    write_t=TRUE, # write transformation value in lower right corner?
@@ -20,25 +22,28 @@ linLogTrans <- function(
 if(is.null(values_t)) # if it's not given by user, use internal calculation:
   {
   if(length(steps)>1) steps <- max(steps, na.rm=TRUE)
+  if(steps>1000) steps <- 1000
   # t-values for x^(1/t):
-  allt <- 10^(seq(0,2.5,len=1e4) )
-  # selection at upper half of these values;
-  # Otherwise, the animation slows down too much at the end
-  f <- 1.4 # multiplication factor due to length loss by unique
-  sel <- round(seq(1, 10, len=f*steps)^4)   #0.5*seq(1, 100, len=1.3*steps)^2 + 0.5*
-  sel2 <- unique(round(log10(seq(1, 10, len=f*steps))*f*steps))
-  sel2[1] <- 1
-  sel <- sel[sel2]
+  # coefficients of degree 17 polynomial, see examples
+  tvc <- c(0.995726006684206, 0.00310820777426466, -2.7073341927363e-05,
+  6.11849831959088e-07, -7.05912829318337e-09, 4.82269115381641e-11,
+  -2.02029859969675e-13, 5.30790764027315e-16, -8.53304767303152e-19,
+  7.29652239791065e-22, -8.04764444262045e-26, -4.35519950517021e-28,
+  3.26048883565918e-31, NA, -6.70898382748396e-38, NA, 1.04885136585542e-44,NA)
+  tvx <- 1:1000 # t x values
+  tvy <- rowSums(sapply(1:18, function(i) tvc[i]*tvx^(i-1)), na.rm=TRUE)
+  tvy[1] <- 1
   # final t-values for transition:
-  allt <- unique(round(allt[sel], 2))
+  allt <- tvy[seq(1,1000, length=steps)]
   }
   else allt <- values_t
-# Plot the images --------------------------------------------------------------
+# return t values only:
 if(!plot) return(allt)
+# Plot the images --------------------------------------------------------------
 # Plot on linear scale first:
 if(firstplot)
   {
-  plot(x, y, las=las, type="n", ...)
+  plot(x, y, las=las, type="n", xlim=xlim, ylim=ylim, ...)
   do.call(points, args=owa(d=list(x=x, y=y), a=pointsarg, "x", "y"))
   }
 # in case people capitalize log:
@@ -51,7 +56,7 @@ if(log=="x") # -----------------------------------------------------------------
   for(t in allt)
      {
      # Plot single frame:
-     plot(x^(1/t), y, las=las, xaxt="n", type="n", ...)
+     plot(x^(1/t), y, las=las, xaxt="n", type="n", xlim=xlim^(1/t), ylim=ylim, ...)
      # draw grey lines at 10^n values and label appropriate ones:
      abline(v=(lv$all)^(1/t), col=8)
      axis(1, (lv$vals)^(1/t), lv$labs, las=las)
@@ -66,7 +71,7 @@ if(log=="x") # -----------------------------------------------------------------
   # Final image
   if(lastplot)
     {
-    plot(x, y, las=las, xaxt="n", type="n", log="x", ...)
+    plot(x, y, las=las, xaxt="n", type="n", log="x", xlim=xlim, ylim=ylim, ...)
     abline(v=lv$all, col=8)
     axis(1, lv$vals, lv$labs, las=las)
     do.call(points, args=owa(d=list(x=x, y=y), a=pointsarg, "x", "y"))
@@ -78,7 +83,7 @@ else if(log=="y") # ------------------------------------------------------------
   lv <- logVals(y, base=base)
   for(t in allt)
      {
-     plot(x, y^(1/t), las=las, yaxt="n", type="n", ...)
+     plot(x, y^(1/t), las=las, yaxt="n", type="n", xlim=xlim, ylim=ylim^(1/t), ...)
      abline(h=(lv$all)^(1/t), col=8)
      axis(2, (lv$vals)^(1/t), lv$labs, las=las)
      pargs <- owa(d=list(x=x, y=y^(1/t)), a=pointsarg, "x", "y")
@@ -88,7 +93,7 @@ else if(log=="y") # ------------------------------------------------------------
      } # End for loop
   if(lastplot)
     {
-    plot(x, y, las=las, yaxt="n", type="n", log="y", ...)
+    plot(x, y, las=las, yaxt="n", type="n", log="y", xlim=xlim, ylim=ylim, ...)
     abline(h=lv$all, col=8)
     axis(2, lv$vals, lv$labs, las=las)
     do.call(points, args=owa(d=list(x=x, y=y), a=pointsarg, "x", "y"))
@@ -101,7 +106,7 @@ else if(log=="xy" | log=="yx") # -----------------------------------------------
   lvy <- logVals(y, base=base)
   for(t in allt)
      {
-     plot(x^(1/t), y^(1/t), las=las, xaxt="n", yaxt="n", type="n", ...)
+     plot(x^(1/t), y^(1/t), las=las, xaxt="n", yaxt="n", type="n", xlim=xlim^(1/t), ylim=ylim^(1/t), ...)
      abline(h=(lvy$all)^(1/t), v=(lvx$all)^(1/t), col=8)
      axis(1, (lvx$vals)^(1/t), lvx$labs, las=las)
      axis(2, (lvy$vals)^(1/t), lvy$labs, las=las)
@@ -112,7 +117,7 @@ else if(log=="xy" | log=="yx") # -----------------------------------------------
      } # End for loop
   if(lastplot)
     {
-    plot(x, y, las=las, xaxt="n", yaxt="n", type="n", log="xy", ...)
+    plot(x, y, las=las, xaxt="n", yaxt="n", type="n", log="xy", xlim=xlim, ylim=ylim, ...)
     abline(h=lvy$all, v=lvx$all, col=8)
     axis(1, lvx$vals, lvx$labs, las=las)
     axis(2, lvy$vals, lvy$labs, las=las)
@@ -124,11 +129,28 @@ else stop("log can only be 'x', 'y', or 'xy'.")
 return(invisible(allt))
 } # end of function ------------------------------------------------------------
 
-
 # old way to get the transformation values:
+#  if(length(steps)>1) steps <- max(steps, na.rm=TRUE)
+#  # t-values for x^(1/t):
+#  allt <- 10^(seq(0,2.5,len=1e4) )
+#  # selection at upper half of these values;
+#  # Otherwise, the animation slows down too much at the end
+#  f <- 1.4 # multiplication factor due to length loss by unique
+#  sel <- round(seq(1, 10, len=f*steps)^4)   #0.5*seq(1, 100, len=1.3*steps)^2 + 0.5*
+#  sel2 <- unique(round(log10(seq(1, 10, len=f*steps))*f*steps))
+#  sel2[1] <- 1
+#  sel <- sel[sel2]
+#  # final t-values for transition:
+#  allt <- unique(round(allt[sel], 2))
+
+# Current t value calculation is based on a polynomial of degree 17 fitted to
+# the t values for 300 steps from this version
+
+# very old way to get the transformation values:
 # make more steps, as ca 35% are removed from allt later:
 # steps <- round(steps * 1.35)#(1+(0.5+0.2*0.5)))
 # steps <- 150
 # allt <- 10^(seq(0,2.5,len=steps) )
 # sel <- round(10^(seq(log10(steps/2), log10(steps), len=0.2*steps) ))
 # allt <- allt[c(1:(sel[1]-1), sel)]
+
