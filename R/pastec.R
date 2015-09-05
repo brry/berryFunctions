96a1668551271e45ed50026a097a9d3250bf38e0
brry <berry-b@gmx.de>
2015-09-05 20:57:44 +0200

    small things in wishlistalready taken care of

--- R/pastec.R
+++ R/pastec.R
@@ -8,3 +8,17 @@ collapse=", " # Character string between combined strings
 {
 paste(..., sep=sep, collapse=collapse)
 }
+
+## Old idea to enable newline insertion every n elements, apparently not necessary.
+##newlines=TRUE # try to add line breaks at suitable intervals
+##\item{newlines}{Add line breaks? (ca. at console width). DEFAULT: TRUE}
+##dd <- paste(..., sep=sep, collapse=collapse)
+##if(newlines)
+##{
+##  w <- getOption("width")
+##  if(nchar(dd) < w) return(dd) 
+##  dd2 <- strsplit(dd, collapse)[[1]]
+##  sel <- cumsum(nchar(dd2)+2) < w
+##  dd3 <- dd2[sel]
+##}
+##return(dd)
