4f64db03a0b4150c06e9b7d9588145eb4a168347
brry <berry-b@gmx.de>
2015-09-20 13:28:30 +0200

    linReg: now adds confidence interval to plot

--- R/linReg.R
+++ R/linReg.R
@@ -10,6 +10,7 @@ linReg <- function(
    add=FALSE,
    pch=16,
    col=2,
+   colband=addAlpha(col), # Color of the confidence region band. DEFAULT: addAlpha(col)
    lwd=1,
    xlab=deparse(substitute(x)),
    ylab=deparse(substitute(y)),
@@ -37,6 +38,9 @@ if (!add) plot(x, y, las=1, pch=pch, xlab=xlab, ylab=ylab, main=main, ...)
 # do linear regression and plotting
 mod <- lm( y ~ x )
 abline(mod, col=col, lwd=lwd, ...)
+x2 <- seqR(par("usr")[1:2], len=100)
+pred <- predict(mod, newdata=data.frame(x=x2), interval="confidence" )
+ciBand(yu=pred[,3], yl=pred[,2], x=x2, colb=colband, add=TRUE)
 # expand digits vector, if necessary
 digits <- rep(digits, length.out=4)
 # Prepare formula writing
