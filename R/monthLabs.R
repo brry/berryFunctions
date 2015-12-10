564a5c5c5d9980b13594a689efd28978fa376ba5
brry <berryboessenkool@hotmail.com>
2015-12-10 20:42:40 +0100

    monthLabs: npy=12 now possible

--- R/monthLabs.R
+++ R/monthLabs.R
@@ -26,9 +26,10 @@ as.Date(   paste(rep(startyear:stopyear, each=12*npm),
 } else
 {
 # check for wrong npy-argument
-if(!npy %in% c(1:4,6)) stop("wrong 'npy'-value: possible are 1,2,3,4 or 6.")
+if(!npy %in% c(1:4,6,12)) stop("wrong 'npy'-value: possible are 1,2,3,4,6 or 12.")
 # define possible combinations
 npyval <- list(npy1=1, npy2=c(1,7), npy3=c(1,5,9), npy4=c(1,4,7,10), npy5=NA, npy6=c(1,3,5,7,9,11))
+npyval[[12]] <- 1:12
 # paste the years and (dependent on npy) the months
 as.Date(paste(rep(startyear:stopyear, each=npy), npyval[[npy]], "01", sep="-"))
 } # end of labels per year
