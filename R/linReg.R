8311d1e814137d64b9ad351413b29c57b4726f77
brry <berryboessenkool@hotmail.com>
2015-12-15 21:32:18 +0100

    linReg: formula arguments and conf.level added. Documentation improved.

--- R/linReg.R
+++ R/linReg.R
@@ -1,21 +1,22 @@
 linReg <- function(
-   x,
-   y=NULL,
-   data=NULL,
-   digits=2,
-   inset=0,
-   xpd=par("xpd"),
-   pos1="top",
-   pos2=NULL,
-   add=FALSE,
-   pch=16,
-   col=2,
-   colband=addAlpha(col), # Color of the confidence region band. DEFAULT: addAlpha(col)
-   lwd=1,
-   xlab=deparse(substitute(x)),
-   ylab=deparse(substitute(y)),
-   main="linear regression",
-   ...)
+x,                # Numeric or formula (see examples). Vector with values of explanatory variable
+y=NULL,           # Numeric. Vector with values of dependent variable.
+data=NULL,        # Dataframe. If x is a formula, the according columns from data are used as x and y
+add=FALSE,        # Logical. If TRUE, line and text are added to the existing graphic. DEFAULT: FALSE (plots datapoints first and then the line.)
+digits=2,         # Numeric vector of length \eqn{\ge 1}. Specifies number of digits a,b,r,e are rounded to in the formula "y=a*x+b \n R^2=r \n RMSE=e", respectively. If values are not specified, they are set equal to the first.
+pch=16,                       # Point Character of datapoints, see \code{\link{par}}
+col=2,                        # Color of the regression line, see \code{\link{par}}
+colband=addAlpha(col),        # Color of the confidence region band. DEFAULT: addAlpha(col)
+level=0.95,                   # Confidence level, see \code{\link{predict.lm}}
+lwd=1,                        # Numeric. Linewidth, see \code{\link{par}}
+xlab=deparse(substitute(x)),  # Axis label if add=FALSE
+ylab=deparse(substitute(y)),  # Axis label if add=FALSE
+main="linear regression",     # Title if add=FALSE. Changed (if not specified) for x=formula with data.
+pos1="top",                   # \code{\link{xy.coords}}-acceptable position of the formula
+pos2=NULL,                    # For numerical coordinates, this is the y-position. DEFAULT: NULL, as in \code{\link{legend}}
+inset=0,                      # Numeric vector of length \eqn{\le 2}. inset distance(s) from the margins as a fraction of the plot region when formula legend is placed by keyword.
+legargs=NULL,                 # list of arguments passed to legend, like list(cex=0.8, xpd=TRUE, bg="white"), ...   xpd specifies whether formula can be written only inside the plot region (when FALSE) or inside the figure region including mar (when TRUE) or in the entire device region including oma (when NA)
+...)                          # Further arguments passed to \code{\link{plot}} and \code{\link{abline}}.
 {
 if(class(x)=="formula")
   {
@@ -33,7 +34,7 @@ if (!add) plot(x, y, las=1, pch=pch, xlab=xlab, ylab=ylab, main=main, ...)
 mod <- lm( y ~ x )
 abline(mod, col=col, lwd=lwd, ...)
 x2 <- seqR(par("usr")[1:2], len=100)
-pred <- predict(mod, newdata=data.frame(x=x2), interval="confidence" )
+pred <- predict(mod, newdata=data.frame(x=x2), interval="confidence", level=level )
 ciBand(yu=pred[,3], yl=pred[,2], x=x2, colb=colband, add=TRUE)
 # expand digits vector, if necessary
 digits <- rep(digits, length.out=4)
@@ -44,5 +45,6 @@ r <- round( summary(mod)$r.squared , digits[3])
 Txt <- paste0("y = ", a, " * x ", ifelse(b>0," + ", " - "), abs(b), "\nR\U00B2 = ",
           r, "\nRMSE = ", signif(rmse(x,y), digits[4]))
 # write formula
-legend(pos1, pos2, Txt, bty="n", text.col=col, inset=inset, xpd=xpd)
+legdef <- list(x=pos1, y=pos2, legend=Txt, bty="n", text.col=col, inset=inset)
+do.call(legend, owa(legdef, legargs, "legend", "inset"))
 } # end of function
