d7d9d699f44c1fedac72cc17048713c6f9cad96c
brry <berryboessenkool@hotmail.com>
2016-02-18 16:10:42 +0100

    rcor: initial release

---
+++ R/rcor.R
@@ -0,0 +1,48 @@
+# spatially correlated random values
+# Berry Boesenkool, Jan 2016, berry-b@gmx.de
+# For regular grid fields, see:
+# http://santiago.begueria.es/2010/10/generating-spatially-correlated-random-fields-with-r/
+
+
+rcor <- function(
+n,      # Number of randomly scattered points to be created
+xmin,   # Smallest x-coordinate
+xmax,   # Largest x-coordinate
+ymin,   # Smallest y-coordinate
+ymax,   # Largest y-coordinate
+allplots=TRUE, # save all plots in dummy.pdf
+zstart, # Vector of starting values for seeding some points, must be shorter than n
+zfun=rnorm, # A function for random noise creation, taking n as first argument
+...)  # Further arguments passed to zfun, like sd=3
+{
+if(allplots) { pdf("dummy.pdf") ; on.exit(dev.off())}
+# coordinate vectors
+x <- runif(n, xmin, xmax)
+y <- runif(n, ymin, ymax)
+z <- rep(NA, n)
+# number of seeded points:
+ns <- length(zstart)
+z[1:ns] <- zstart
+# browser()
+#### distances for each seeded point:
+# compute autocorrelated z values:
+for(i in (ns+1):n)
+{
+if(allplots) colPoints(x,y,z, add=FALSE, asp=1)
+# get value from closest populated point and add noise
+d <- distance(x[i],y[i], x,y)
+d[is.na(z)] <- NA
+z[i] <- z[which.min(d)] + zfun(n=1, ...)
+}
+if(allplots) colPoints(x,y,z, add=FALSE, asp=1)
+# function output
+data.frame(x,y,z)
+}
+
+if(FALSE){
+coor <- rcor(n=100, xmin=45, xmax=80, ymin=230, ymax=270, zstart=c(20,30,40), sd=3)
+colPoints(x,y,z, data=coor, add=FALSE, asp=1, col=divPal(100))
+points(coor$x[1:3], coor$y[1:3], cex=1.4)
+
+}
+
