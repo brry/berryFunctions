f98987391258168193abe29a58a90948b77d67f8
brry <berryboessenkool@hotmail.com>
2016-05-18 18:12:38 +0200

    approx2: can now use custom n

--- R/approx2.R
+++ R/approx2.R
@@ -7,10 +7,7 @@
 #' @details The function fill is used to fill missing values at the ends of the vector.
 #' It could be mean or median, for example, but must be a function that accepts \code{na.rm=TRUE} as an argument. 
 #' The default (NULL) means to use the first (or last) observation available.
-#' 
-#' @param x Vector with (numeric) values
-#' @param fill Function to fill NAs at the start or end of the vector. See Details. DEFAULT: NULL
-
+#'
 #' @return Vector with NAs replaced with interpolation (not a list, as in \code{\link{approx}}!)
 #' @author Berry Boessenkool, \email{berry-b@@gmx.de}, July 2015
 #' @seealso \code{\link{approx}}, zoo::na.locf, \code{\link{ciBand}} for usage example
@@ -18,10 +15,10 @@
 #' @export
 #' @examples
 #' 
-#' # approx2(c(NA,NA)) # yields an error
+#' is.error(  approx2(c(NA,NA))  ) # yields an error
 #' approx2(c(NA,NA, 6, 4, 8, 9, 3, 2, 1))
-#' approx2(c( 3,NA, 6, 4, 8, 9, 3, 2, 1))
-#' approx2(c( 3, 4, 6, 4, 8, 9,NA, 2,NA))
+#' approx2(c( 2,NA, 6, 4, 8, 9, 3, 2, 1))
+#' approx2(c( 2, 4, 6, 4, 8, 9,NA, 2,NA))
 #' 
 #' approx2(c(NA,NA, 6, 4, 8, 9, 3, 2, 1))
 #' approx2(c(NA,NA, 6, 4, 8, 9, 3, 2, 1), fill=median)
@@ -31,19 +28,30 @@
 #' approx2(c( 3, 4, 6, 4, 8, 9,NA, 2,NA), fill=median)
 #' approx2(c( 3, 4, 6, 4, 8, 9,NA, 2,NA), fill=mean)
 #' 
+#' approx2(c(NA,NA, 6, 4, 8, 9, 3, 2, 1), n=17)
+#' approx2(c( 2,NA, 6, 4, 8, 9, 3, 2, 1), n=17)
+#' approx2(c( 2, 4, 6, 4, 8, 9,NA, 2,NA), n=17)
+#'
+#' @param x Vector with (numeric) values
+#' @param fill Function to fill NAs at the start or end of the vector. See Details. DEFAULT: NULL
+#' @param n Number of points to interpolate to
+#' @param \dots Further arguments passed to \code{\link{approx}}
+
 approx2 <- function(
 x, 
-fill=NULL 
+fill=NULL,
+n=length(x),
+...
 )
 {
 # Input controls
 if(all(is.na(x))) stop("There are no non-NA values in x.")
-n <- length(x)
+L <- length(x)
 # Fill leading NAs (with the first available value):
 if(is.na(x[1])) x[1] <- if(is.null(fill)) head(x[!is.na(x)],1) else fill(x, na.rm=TRUE)
 # Fill trailing NAs (with the last available value):
-if(is.na(x[n])) x[n] <- if(is.null(fill)) tail(x[!is.na(x)],1) else fill(x, na.rm=TRUE)
+if(is.na(x[L])) x[L] <- if(is.null(fill)) tail(x[!is.na(x)],1) else fill(x, na.rm=TRUE)
 # Fill central NAs with a linear interpolation from the surrounding values:
-approx(x, n=n)$y
+approx(x, n=n, ...)$y
 }
 
