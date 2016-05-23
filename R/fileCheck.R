535caa9e4ac3e500d9c0d1e59a108551c5061895
brry <berryboessenkool@hotmail.com>
2016-05-23 18:37:16 +0200

    fileCheck: can now take a vector of files, print the calling stack, and has more examples in the function documentation

--- R/fileCheck.R
+++ R/fileCheck.R
@@ -12,16 +12,55 @@
 #' fileCheck("FileThatDoesntExist.txt", fun=warning)
 #' fileCheck("FileThatDoesntExist.txt", fun=message)
 #' is.error( fileCheck("FileThatDoesntExist.txt", fun=MyWarn) ) # nonexisting function
+#' 
+#' \dontrun{## Excluded from CRAN checks because of file creation
+#' # Vectorized:
+#' file.create("DummyFile2.txt")
+#' fileCheck(paste0("DummyFile",1:3,".txt"), fun=message)
+#' fileCheck(paste0("DummyFile",1:3,".txt") ) 
+#' file.remove("DummyFile2.txt")
+#' }
+#' 
+#' \dontrun{## Excluded from CRAN checks because of intentional errors
+#' compareFiles("dummy.nonexist", "dummy2.nonexist")
+#' fileCheck("dummy.nonexist")
 #'
-#' @param file Filename as character string to be checked for existence.
+#' dingo <- function(k="brute.nonexist") fileCheck(k)
+#' dingo()
+#' dingo("dummy.nonexist")
+#'
+#' upper <- function(h) dingo(c(h, "dumbo.nonexist"))
+#' upper("dumbo2.nonexist")
+#' }
+#'
+#' @param file Filename(s) as character string to be checked for existence.
 #' @param fun One of the functions \code{\link{stop}}, \code{\link{warning}}, or \code{\link{message}}. DEFAULT: stop
+#' @param trace Logical: Add function call stack to the message? DEFAULT: TRUE
+#' @param \dots Further arguments passed to \code{fun}
 #'
 fileCheck <- function(
 file,
-fun=stop
+fun=stop,
+trace=TRUE,
+...
 )
 {
+# tracing the calling function(s):
+if(trace)
+  {
+  dummy <- capture.output(tb <- traceback(6) )
+  calltrace <- sapply(strsplit(unlist(tb), "(", fixed=TRUE), "[", 1)
+  calltrace <- paste(rev(calltrace), collapse=" -> ")
+  }
+# check actual file existence:
 exi <- file.exists(file)
-if(!exi) fun("  The file '", file, "'\n  does not exist at ", getwd() )
+# prepare message:
+Text <- if(length(exi)>1)
+paste0("  The files '", pastec(file[!exi]), "'\n  do not exist at ", getwd()) else
+paste0("  The file '",         file,      "'\n  does not exist at ", getwd())
+#
+if(trace) Text <- paste(calltrace, Text, sep="\n")
+# return message, if file nonexistent:
+if(any(!exi)) fun(Text, ...)
 return(invisible(exi))
 }
