25ce4485e4e73c3561cb1567fa5cd2fabf31be32
brry <berryboessenkool@hotmail.com>
2016-05-24 14:36:23 +0200

    is.error: gains argument 'force'. If force=TRUE, is.error returns an error if the expression does not return an error.

--- R/is.error.R
+++ R/is.error.R
@@ -1,6 +1,7 @@
 #' Check if an expression returns an error
 #'
 #' Does a given expression return an error?
+#' Useful for tests where you want to make sure your function throws an error.
 #'
 #' @return TRUE/FALSE
 #' @author Berry Boessenkool, \email{berry-b@@gmx.de}, May 2016
@@ -10,18 +11,28 @@
 #' @examples
 #' is.error(  log(3)              )
 #' is.error(  log("a")            )
+#' is.error(  log(3),   tell=TRUE )
 #' is.error(  log("a"), tell=TRUE )
-#' stopifnot( is.error( log("a")  )  )
+#' stopifnot( is.error( log("a")  )  ) # or shorter:
+#' is.error(  log("a"), force=TRUE) 
+#' # is.error(  log(3),   force=TRUE)
+#' stopifnot(is.error(  is.error(log(3), force=TRUE)  ))
 #'
 #' @param expr Expression to be tested for retunrning an error
 #' @param tell Logical: Should the error message be printed via \code{\link{message}}? DEFAULT: FALSE
+#' @param force Logical: Should an error be returned if the expression is not an error? DEFAULT: FALSE
 #'
 is.error <- function(
 expr,
-tell=FALSE
+tell=FALSE,
+force=FALSE
 )
 {
+expr_name <- deparse(substitute(expr))
 test <- try(expr, silent=TRUE)
-if(tell) message("Note in is.error: ", test)
-inherits(test, "try-error")
+iserror <- inherits(test, "try-error")
+if(tell) if(iserror) message("Note in is.error: ", test)
+if(force) if(!iserror) stop(expr_name, " is not returning an error.", call.=FALSE)
+# output:
+iserror
 }
