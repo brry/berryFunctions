34ab56977ecb2f59299746757ea2672a19f917da
brry <berryboessenkool@hotmail.com>
2016-07-05 15:57:00 +0200

    googleLink2pdf: more link subsets discarded

--- R/googleLink2pdf.R
+++ R/googleLink2pdf.R
@@ -24,6 +24,7 @@ googlelink
 {
 pdflink <- strsplit(googlelink, "&url=")[[1]][2]
 pdflink <- strsplit(pdflink, "&ei=")[[1]][1]
+pdflink <- strsplit(pdflink, "&usg=")[[1]][1]
 pdflink <- gsub("%24", "$", pdflink)
 pdflink <- gsub("%2F", "/", pdflink)
 pdflink <- gsub("%3A", ":", pdflink)
