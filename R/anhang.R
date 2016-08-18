a38a6382069b9f2936a3f908af30cca617fd66f1
brry <berryboessenkool@hotmail.com>
2016-08-18 17:36:41 +0200

    anhang: made generally usable

--- R/anhang.R
+++ R/anhang.R
@@ -2,14 +2,14 @@
 #'
 #' Open the Appendix of my R handbook found online at
 #' \url{RclickHandbuch.wordpress.com} or directly at
-#' \url{https://dl.dropboxusercontent.com/u/4836866/Rclick/Anhang.pdf}.
-#' This function is rather for internal usage on my two computers.
+#' \url{https://dl.dropbox.com/u/4836866/Rclick/Anhang.pdf}.
 #'
-#' @return None, opens pdf in default viewer using \code{\link{system2}}
+#' @return None, opens pdf in default viewer using \code{\link{system2}} or \code{\link{browseURL}}
 #' @author Berry Boessenkool, \email{berry-b@@gmx.de}, Jul 2016
 #' @seealso \code{\link{funSource}}
 #' @export
 #' @examples
+#' # anhang() # excluded from cran check because of external browser opening policy
 #'
 anhang <- function()
 {
@@ -19,7 +19,9 @@ if(!file.exists(file)) file <- gsub("S:", "~", file)
 # work PC path change:
 if(!file.exists(file)) file <- gsub("~", "C:/Users/boessenkool", file)
 # path control
-checkFile(file)
+# checkFile(file)
 # open pdf
-system2("open", file)
+if(file.exists(file)) system2("open", file)
+else
+browseURL("https://dl.dropbox.com/u/4836866/Rclick/Anhang.pdf")  
 }
