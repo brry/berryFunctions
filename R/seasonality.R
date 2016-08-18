086b5177e717f78b29d5f0250bbc9a03fb1a3de4
brry <berryboessenkool@hotmail.com>
2016-08-18 17:33:19 +0200

    seasonality: annual maxima included

--- R/seasonality.R
+++ R/seasonality.R
@@ -2,7 +2,9 @@
 #'
 #' Plot time series to examine it for seasonality
 #'
-#' @return ReturnValue
+#' @return Data.frame with DOYs of annual maxima and number of nonNA values included
+#'         Please Note that the column year does not note the calendrical year 
+#'         if \code{shift!=0}. 
 #' @author Berry Boessenkool, \email{berry-b@@gmx.de}, Jul 2016
 #' @seealso \code{\link{spiralDate}}
 #' @keywords aplot
@@ -14,6 +16,7 @@
 #' rm(qfile)
 #' colnames(Q) <- c("date","discharge")
 #' Q$date <- as.Date(Q$date)
+#' Q$discharge[530:540] <- NA
 #' plot(Q, type="l")
 #' spiralDate(date, discharge, data=Q) # most floods in winter
 #' seasonality(date, discharge, data=Q, shift=100, main="NRFA: Thames\nRoyal Windsor Park")
@@ -27,10 +30,12 @@
 #'               can be a vector like \code{dates}. DEFAULT: NULL
 #' @param vrange Optional value range (analogous to ylim), 
 #'               can be a vector like \code{values}. DEFAULT: NULL
-#' @param shift Number of days to move the year-break to. DEFAULT: 0
+#' @param shift Number of days to move the year-break to. 
+#'              E.g. shift=61 for German hydrological year (Nov to Oct). DEFAULT: 0
 #' @param janline Logical: Should horizontal line be plotted at 
 #'                January 1st if \code{shift!=0}? DEFAULT: TRUE
-#' @param months Labels for the months
+#'### @param nmax Number of annual maxima to be marked and returned. DEFAULT: 0
+#' @param months Labels for the months. DEFAULT: J,F,M,A,M,J,J,A,S,O,N,D
 #' @param xlab,ylab Labels for the axes. DEFAULT: Year, Month
 #' @param zlab Title of \code{\link{colPointsLegend}}. DEFAULT: \code{values} name
 #' @param ylim,yaxs Parameters specifying y Axis appearance, see \code{\link{par}}. 
@@ -51,7 +56,8 @@ seasonality <- function(
   vrange=NULL,
   shift=0,
   janline=TRUE,
-  months=c("J","F","M","A","M","J","J","A","S","O","N","D"),
+###  nmax=0,
+  months=substr(month.abb,1,1),
   xlab="Year",
   ylab="Month",
   zlab=substitute(values),
@@ -94,19 +100,28 @@ seasonality <- function(
   shift <- abs(shift)
   if(shift>366) stop("'shift' is", shift, ", but should be between 0 and 366.")
   dates <- dates + shift
-  # Actual plotting
+  # Margin parameters
   op <- par(mar=mar, mgp=mgp)
   if(!keeppar) on.exit(par(op))
+  # axis values
   year <- as.numeric(format(dates,"%Y"))
   DOY  <- as.numeric(format(dates,"%j")) # Day of Year
+  # Actual plotting
   colPoints(year, DOY, values, Range=vrange, add=FALSE, zlab=zlab,
             ylab=ylab, xlab=xlab, yaxt="n", ylim=ylim, yaxs=yaxs, ...)
+  # Axis labelling
   labs <- monthLabs(2004,2004, npm=1) + shift
   lDOY  <- as.numeric(format(labs,"%j"))
   if(janline & shift!=0) abline(h=shift+1)
   axis(2, lDOY, months, las=1)
   title(main=main, adj=0)
+  # Annual maxima
+  annmax <- tapply(X=values, INDEX=year, FUN=which.max)# FUN=order, decreasing=TRUE)
+  annmax <- data.frame(year=as.numeric(names(annmax)), DOY=annmax)
+  rownames(annmax) <- NULL
+  year <- year[!is.na(values)]
+  annmax$n <- table(year)
+###  lines(annmax, type="o")
+  return(annmax)
   }
 
-#spiralDate(date, discharge, data=Q) 
-#seasonality(date, discharge, data=Q)
