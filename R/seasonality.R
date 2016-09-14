02c0e02aceabde1c0e9a4f2d26cccf4c95f71a51
brry <berryboessenkool@hotmail.com>
2016-09-14 16:58:49 +0200

    seasonality: nmax lines added to graphs, a possible failure with short time series prevented

--- R/seasonality.R
+++ R/seasonality.R
@@ -3,7 +3,7 @@
 #' Plot time series to examine it for seasonality
 #'
 #' @return Data.frame with \code{year}, \code{n}umber of nonNA entries, 
-#'         \code{max} value + \code{DOY} of annual maxima.
+#'         \code{max} value + \code{doy} of annual maxima.
 #'         Please note that the column year does not note the calendrical year 
 #'         if \code{shift!=0}. 
 #' @author Berry Boessenkool, \email{berry-b@@gmx.de}, Jul 2016
@@ -26,12 +26,17 @@
 #' seas <- seasonality(date, discharge, data=Q, plot=2) # most floods in winter
 #' seas <- seasonality(date, discharge, data=Q, plot=3)
 #' seas <- seasonality(date, discharge, data=Q, plot=3, shift=100)
+#' seasonality(date, discharge, data=Q[200:300,], plot=3, nmax=1)
+#' seasonality(date, discharge, data=Q[100:200,], plot=3, nmax=1, shift=100)
 #' 
-#' #    dev.new(noRStudioGD=TRUE, record=TRUE)     # large graph on 2nd monitor
+#' \dontrun{
+#' dev.new(noRStudioGD=TRUE, record=TRUE)     # large graph on 2nd monitor
 #' par(mfrow=c(2,2))
 #' seas <- seasonality(date, discharge, data=Q, plot=1:4, shift=100)
 #' seas <- seasonality(date, discharge, data=Q, plot=1:4, lwd=2)
+#' seas <- seasonality(date, discharge, data=Q, plot=1:4, nmax=1, shift=100)
 #' seas <- seasonality(date, discharge, data=Q, plot=1:4, col=divPal(100, ryb=TRUE))
+#' }
 #' 
 #' @param dates Dates in ascending order. 
 #'              Can be charater strings or \code{\link{strptime}} results, 
@@ -46,12 +51,15 @@
 #'              E.g. shift=61 for German hydrological year (Nov to Oct). DEFAULT: 0
 #' @param janline Logical: Should horizontal line be plotted at 
 #'                January 1st if \code{shift!=0}? DEFAULT: TRUE
-#'### @param nmax Number of annual maxima to be marked and returned. DEFAULT: 0
-#' @param plot Integer specifying the type of plot. \cr 
+#' @param nmax Number of annual maxima to be marked, plotted and returned. 
+#'             Currently, only 0 and 1 are implemented. DEFAULT: 0
+#' @param maxargs List of arguments passed to \code{\link{lines}} for annual maxima,
+#'                e.g. \code{maxargs=list(type="l", col="red", lty=3)}. DEFAULT: NULL
+#' @param plot Integer specifying the type of plot. Can be a vector to produce several plots. \cr 
 #'             0: none, only data.frame with annual maxima. \cr
-#'             1: color coded DOY (day of the year) over year (the default). \cr
+#'             1: color coded doy (day of the year) over year (the default). \cr
 #'             2: Color coded spiral graph with \code{\link{spiralDate}}. \cr
-#'             3: Spaghetti line plot with discharge over DOY, one line per year. \cr
+#'             3: Spaghetti line plot with discharge over doy, one line per year. \cr
 #'             4: plot of annmax over time for crude trend analysis. \cr
 #'             DEFAULT: 1
 #' @param months Labels for the months. DEFAULT: J,F,M,A,M,J,J,A,S,O,N,D
@@ -59,8 +67,8 @@
 #'                       Note that these are switched in plot 3 and 4.
 #'                       DEFAULT: Year, Month, substitute(values)
 #' @param ylim Limits of y axis. DEFAULT: NA (specified internally per plot type)
-#' @param xaxs,yays x and y Axis style, see \code{\link{par}}. 
-#'                DEFAULT: "r" (regular 4% expansion), "i" (internal range only)
+#' @param xaxs,yaxs x and y Axis style, see \code{\link{par}}. 
+#'                DEFAULT: "r" (regular 4\% expansion), "i" (internal range only)
 #' @param main,adj Graph title and offset to the left 
 #'              (\code{adj} passsed to \code{\link{title}}). DEFAULT: "Seasonality", 0.2
 #' @param mar,mgp Parameters specifying plot margin size and labels placement.
@@ -79,8 +87,9 @@ seasonality <- function(
   vrange=NULL,
   shift=0,
   janline=TRUE,
+  nmax=0,
+  maxargs=NULL,
   plot=1,
-###  nmax=0,
   months=substr(month.abb,1,1),
   xlab="Year",
   ylab="Month",
@@ -134,17 +143,18 @@ if(shift>366) stop("'shift' is", shift, ", but should be between 0 and 366.")
 dates <- dates + shift
 # axis values
 year <- as.numeric(format(dates,"%Y"))
-DOY  <- as.numeric(format(dates,"%j")) # Day of Year
+doy  <- as.numeric(format(dates,"%j")) # Day of Year
 #
 # COMPUTATION
 #
 # Annual maxima data.frame
-annmax <- tapply(X=values, INDEX=year, FUN=function(x) sum(!is.na(x)))# FUN=order, decreasing=TRUE)
+annmax <- tapply(X=values, INDEX=year, FUN=function(x) sum(!is.na(x)))
 annmax <- data.frame(year=as.numeric(names(annmax)), n=annmax)
 rownames(annmax) <- NULL
 annmax$max <- tapply(X=values, INDEX=year, FUN=max, na.rm=TRUE)
-annmax$DOY <- tapply(X=values, INDEX=year, FUN=which.max)
-###annmax <- as.data.frame(apply(annmax, 2, as.vector))
+annmax$doy <- tapply(X=values, INDEX=year, FUN=which.max)
+annmax$index <- tapply(X=values, INDEX=year, FUN=length)
+annmax$index <- c(0,head(cumsum(annmax$index),-1)) + annmax$doy
 ### nmax for secondary, tertiary, ... maxima. with new function for event separation
 #
 # PLOTTING
@@ -154,19 +164,19 @@ op <- par(mar=mar, mgp=mgp)
 if(!keeppar) on.exit(par(op))
 # Axis labelling
 labs <- monthLabs(2004,2004, npm=1) + shift
-lDOY <- as.numeric(format(labs,"%j"))
+ldoy <- as.numeric(format(labs,"%j"))
 # Actual plotting
 #
-if(1 %in% plot) # DOY ~ year, col=Q
+if(1 %in% plot) # doy ~ year, col=Q
 {
   if(isnaylim) ylim <- c(370,-3)
-  colPoints(year, DOY, values, Range=vrange, add=FALSE, zlab=zlab,
+  colPoints(year, doy, values, Range=vrange, add=FALSE, zlab=zlab,
             ylab=ylab, xlab=xlab, yaxt="n", ylim=ylim, yaxs=yaxs, ...)
   # Axis labelling
   if(janline & shift!=0) abline(h=shift+1)
-  axis(2, lDOY, months, las=1)
+  axis(2, ldoy, months, las=1)
   title(main=main, adj=adj)
-  ###  if(annmax) lines(annmax$year, annmax$DOY, type="o")
+  if(nmax==1) do.call(lines, owa(list(x=annmax$year, y=annmax$doy, type="o"), maxargs))
 }
 #
 if(2 %in% plot) # Spiral graph, col=Q
@@ -175,6 +185,9 @@ if(2 %in% plot) # Spiral graph, col=Q
              months=months, shift=shift, ...)
   title(main=main, adj=adj)
   if(janline) segments(x0=0, y0=0, x1=sin(shift/365.25*2*pi), y1=cos(shift/365.25*2*pi))
+  if(missing(nmax)) nmax <- 0 # looks really ugly
+  if(nmax==1) do.call(lines, owa(list(x=spd[annmax$index,"x"], 
+                                      y=spd[annmax$index,"y"], type="o"), maxargs))
 }
 # parameters for both next plots
 if(missing(mar)) par(mar=c(3,4,4,1))
@@ -182,19 +195,21 @@ if(missing(mgp)) mgp <- c(2.7,0.7,0)
 if(isnaylim) ylim <- range(values, na.rm=TRUE)
 if(missing(yaxs)) yaxs <- "r"
 #
-if(3 %in% plot) # Q~DOY, col=year
+if(3 %in% plot) # Q~doy, col=year
 {
   # date year range
   if(!exists("drange3", inherits=FALSE)) drange3 <- range(year)
   xaxs3 <- if(missing(xaxs)) "i" else xaxs
   # NAs between years
-  data3 <- data.frame(DOY, values, year)
-  separators <- which(diff(year)!=0)
-  separators <- separators + 1:length(separators)
-  ### check what happens if only one year of data is given
-  data3 <- insertRows(data3, separators)
+  data3 <- data.frame(doy, values, year)
+  if(diff(range(year, na.rm=TRUE))>0)
+    {
+    separators <- which(diff(year)!=0)
+    separators <- separators + 1:length(separators)
+    data3 <- insertRows(data3, separators)
+    }
   # plot
-  colPoints(DOY, values, year, data=data3, Range=drange3, add=FALSE, zlab=xlab,
+  colPoints(doy, values, year, data=data3, Range=drange3, add=FALSE, zlab=xlab,
             ylab="", xlab=ylab, xaxt="n", legargs=list(density=FALSE), 
             ylim=ylim, yaxs=yaxs, lines=TRUE, nint=1, xaxs=xaxs3,
             if(!exists("col", inherits=FALSE)) col=seqPal(100, colors=c("red","blue")), 
@@ -202,22 +217,24 @@ if(3 %in% plot) # Q~DOY, col=year
   mtext("") # weird behaviour: title not added without this line. ### Is this a bug?
   title(ylab=zlab, mgp=mgp)
   # Axis labelling
-  axis(1, lDOY, months, las=1)
+  axis(1, ldoy, months, las=1)
   title(main=main, adj=adj)  
   if(janline & shift!=0) abline(v=shift+1)
+  if(nmax==1) do.call(lines, owa(list(x=doy[annmax$index], y=values[annmax$index],
+                                    type="p", pch=21, cex=1.5, lwd=2, bg="white"), maxargs))
 }
 #
 if(4 %in% plot) # annmax~year, col=n
 {
   if(missingzlab) zlab <- paste("Annual max", zlab)
-  colPoints(year, max, n, data=annmax, add=FALSE, 
+  colPoints(year, max, "n", data=annmax, add=FALSE, 
             ylab="", xlab=xlab, legargs=list(density=FALSE), lines=TRUE, ...)
   mtext("") ### as above
   title(ylab=zlab, mgp=mgp)
   if(missing(main)) main <- "Trend of annual maxima"
   title(main=main, adj=adj)  
+  ### nmax once larger values are possible
 }
-### nmax for each plot method
 #
 # Function output
 return(annmax)
