3ac664cf6c6b6906e9e9edee26610cdbdfc07264
brry <berryboessenkool@hotmail.com>
2016-11-19 16:13:29 +0100

    convertUmlaut: initial release

---
+++ R/convertUmlaut.R
@@ -0,0 +1,35 @@
+#' Convert German Umlaute to ASCII
+#'
+#' Convert German Umlaute (ae, oe, ue, ss) to ASCII.
+#' Conversion happens case sensitive for the first three.
+
+#' @return Character strings
+#' @author Berry Boessenkool, \email{berry-b@@gmx.de}, Oct-Nov 2016
+#' @seealso \code{tools::\link[tools]{showNonASCII}}, \code{\link{gsub}}
+#' @keywords character
+#' @export
+#' @examples
+#' \dontrun{
+#' link <- paste0("ftp://ftp-cdc.dwd.de/pub/CDC/observations_germany/climate/",
+#'                "monthly/kl/recent/KL_Monatswerte_Beschreibung_Stationen.txt")
+#' weatherstations <- read.fwf(link, widths=c(6,9,10,16,11,8,41,99), skip=3)
+#' examples <- removeSpace(weatherstations[c(153, 509, 587, 2, 651, 851),7])
+#' examples
+#' convertUmlaut(examples) # note how lower and upper case is kept
+#' }
+#'
+#' @param x Character string(s) containing German Umlaute
+#'
+convertUmlaut <- function(
+x
+)
+{
+x <- gsub("\U00E4", "ae", x)
+x <- gsub("\U00F6", "oe", x)
+x <- gsub("\U00FC", "ue", x)
+x <- gsub("\U00DF", "ss", x)
+x <- gsub("\U00C4", "Ae", x)
+x <- gsub("\U00D6", "Oe", x)
+x <- gsub("\U00DC", "Ue", x)
+x
+}
