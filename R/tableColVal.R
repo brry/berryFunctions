b95e2e67e80b8dd0567568d36a1c6f3898d98934
brry <berryboessenkool@hotmail.com>
2016-11-22 23:33:47 +0100

    tableColVal: hugely simplified, ellipsis passed to all text, locations improved

--- R/tableColVal.R
+++ R/tableColVal.R
@@ -3,14 +3,10 @@
 #' Table with numbers and corresponding color in the background of each cell.
 #' (heatmap)
 #' 
-#' @details I saw a presentation today with a table of values of differences between several
-#' models and datasets of global precipitation. I decided I don't like reading
-#' 20+ values, and would like to see a corresponding color in the background of each cell. (heatmap)
-#' Writing this function took me about 1 hour and 30 minutes and was a nice coding excercise.
-#' Feedback welcome at berry-b@gmx.de!
+#' @details Create tables with corresponding color in the background of each cell. (heatmap)
 #' 
-#' @return None. PDF or plot produced.
-#' @author Berry Boessenkool, \email{berry-b@@gmx.de}, Nov. 2012
+#' @return List of locations in plot. 
+#' @author Berry Boessenkool, \email{berry-b@@gmx.de}, Nov 2012 + Nov 2016
 #' @seealso \code{\link{pdf}}, \code{\link{heatmap}}
 #' @keywords hplot
 #' @importFrom grDevices dev.off pdf
@@ -23,83 +19,83 @@
 #' rownames(Bsp) <- paste("prod", 8:11, sep="_")
 #' Bsp
 #' 
-#' tableColVal(Bsp)
+#' (  tableColVal(Bsp)   )
 #' tableColVal(Bsp, nameswidth=0.1) # relative to plot width
-#' tableColVal(Bsp, namesheight=0.5, argcol=list(srt=90))
+#' tableColVal(Bsp, namesheight=0.5, srt=45)
 #' 
-#' tableColVal(Bsp, argrow=list(col="red", cex=2) )
+#' tableColVal(Bsp, cellargs=list(cex=2), col="red")
 #' tableColVal(Bsp, Range=c(10,40))
 #' tableColVal(Bsp, Range=c(20,40))
 #' tableColVal(Bsp, palette=heat.colors(12))
-#' tableColVal(Bsp, palette=c(2,4,7), argmain=list(labels="last\ncomparison"))
+#' tableColVal(Bsp, palette=c(2,4,7), main="more\nstuff")
+#' 
+#' Bsp2 <- matrix(rexp(30), ncol=6, byrow=TRUE)
+#' (  tableColVal(Bsp2)  )
+#' tableColVal(Bsp2, digits=4)
+#' colPointsLegend(Bsp2, horizontal=FALSE, x1=0.05, x2=0.15, y1=0.1, y2=0.8, title="")
 #' 
 #' \dontrun{
 #' ## Rcmd check --as-cran doesn't like to open external devices such as pdf,
 #' ## so this example is excluded from running in the checks.
-#' tableColVal(Bsp, pdf=TRUE, width=12)  # further arguments to pdf possible.
-#' 
-#' Bsp2 <- matrix(sample(1:100, 30), ncol=6, byrow=TRUE)
-#' graphics.off(); X11(height=4)
-#' tableColVal(Bsp2)
+#' pdf("TableColVal.pdf", height=5);  tableColVal(Bsp);  dev.off()
+#' system2("open", "TableColVal.pdf")
+#' unlink("TableColVal.pdf")
 #' }
 #' 
-#' @param mat Matrix with values
-#' @param pdffile Name of File to write to. DEFAULT: "table_col_val.pdf"
-#' @param pdf Should table be written to pdffile? (Else it will plot in x11). DEFAULT: FALSE
-#' @param nameswidth Relative width of row names at the left, as a percentage of plot. DEFAULT: 0.3
+#' @param mat         Matrix with values and row/column names
+#' @param main        Title for topleft space. DEFAULT: name of mat object.
+#' @param nameswidth  Relative width of row names at the left, as a percentage of plot. 
+#'                    DEFAULT: 0.3
 #' @param namesheight Relative height of column names at the top. DEFAULT: 0.1
-#' @param palette Color palette for the heatmap. DEFAULT: seqPal(nrow(mat)*ncol(mat))
-#' @param Range Range of values mapped linearly to color palette. DEFAULT: range(mat,finite
-#' @param argclass List of arguments specifying how to call \code{\link{classify}}, eg. method. DEFAULT: NULL
-#' @param argrow,argcol,argcell,argmain List of arguments passed to \code{\link{text}} 
-#'        in row and column names, cell content and topleft cell, respectively. Could be cex, col, srt, etc. DEFAULTS: NULL
-#' @param \dots Further arguments passed to \code{\link{pdf}}.
+#' @param palette     Color palette for the heatmap. DEFAULT: \code{\link{seqPal}(100)}
+#' @param Range       Range mapped to color palette. DEFAULT: range(mat)
+#' @param digits      Number of digits rounded to for writing. DEFAULT: 2
+#' @param classargs   List of arguments specifying how to call \code{\link{classify}}, 
+#'                    e.g. method. DEFAULT: NULL
+#' @param cellargs    List of arguments passed to \code{\link{text}} only for the cells. 
+#' @param \dots Further arguments passed to all \code{\link{text}} like cex, col, srt, ...
 #' 
 tableColVal <- function(
 mat,
-pdffile="table_col_val.pdf",
-pdf=!missing(pdffile),
+main=deparse(substitute(mat)),
 nameswidth=0.3,
 namesheight=0.1,
-palette=seqPal(nrow(mat)*ncol(mat)),
+palette=seqPal(100),
 Range=range(mat,finite=TRUE),
-argclass=NULL,
-argrow=NULL,
-argcol=NULL,
-argcell=NULL,
-argmain=NULL,
+digits=2,
+classargs=NULL,
+cellargs=NULL,
 ...)
 {
-# expand pdf-path to working directory if only file name (without path) is given:
-if(pdf){   if(!grepl("/", pdffile) | grepl("\\", pdffile, fixed=TRUE) )
-               pdffile <- paste(getwd(), pdffile, sep="/")  #"
-        pdf(pdffile, ...) }# open pdf device
+main <- main # evaluate promise before mat is evaluated
 mat <- as.matrix(mat)
-nc <- ncol(mat) ; nr <- nrow(mat)
+nc <- ncol(mat) 
+nr <- nrow(mat)
 # set plot
-op <- par(mai=c(0, 0, namesheight*par()$pin[2], 0), xpd=TRUE )
-plot(1, ylim=c(nr+1, 1), xlim=c(0,1), type="n", xaxs="i", yaxs="i", axes=FALSE, ann=FALSE)
+op <- par(mar=rep(0,4))
+on.exit(par(op))
+plot(1, type="n", xlim=0:1, ylim=0:1, xaxs="i", yaxs="i", axes=FALSE, ann=FALSE)
 # set positions for text and lines
-rights <- seq(nameswidth, 1, len=nc+1)
-lefts <- c(0, rights[1:nc] )
-middles <- nameswidth + (1:nc*2-1) * (1-nameswidth)/nc/2
+x2 <- seq(from=nameswidth,  to=1, len=nc+1) # right ends of cells
+x1 <- c(0, x2[1:nc] )                       # left
+xm <- colMeans(rbind(x1,x2))                # horizontal centers
+y2 <- seq(from=1-namesheight, to=0, len=nr+1) # top ends of cells
+y1 <- c(1, y2[1:nr])                          # bottom
+ym <- colMeans(rbind(y1,y2))                  # vertical centers
 # define color for each value of mat
-cl <- do.call(classify, args=owa(list(x=mat, breaks=length(palette), Range=Range), argclass))
-##mod <- lm(c(1, length(palette)) ~ Range)[[1]]
-##lincol <- round(as.vector(mat) * mod[2] + mod[1])
+cl <- do.call(classify, args=owa(list(x=mat, breaks=length(palette), Range=Range), classargs))
 # plot rectancles with colors corresponding to values of mat
-rect(xleft=rep(lefts[-1], each=nr), xright=rep(rights[-1], each=nr),
-     ybottom=rep(2:(nr+1), nc), ytop=rep(1:nr, nc), col=palette[cl$index] , border=NA)
-abline(v=rights, h=1:nr)
+rect(xleft=rep(x1[-1], each=nr), xright=rep(x2[-1], each=nr), border=NA,
+      ytop=rep(y1[-1], nc),     ybottom=rep(y2[-1], nc), col=palette[cl$index])
+abline(v=c(x1,1), h=c(y1,1))
+# add text to each cell:
+def <- list(x=rep(xm[-1], each=nr), y=rep(ym[-1], nc), labels=round(mat, digits))
+def2 <- list(...)
+do.call(text, args=owa(c(def,def2), cellargs))
 # add "titles"
-ytitles <- 1-(namesheight*nr/2)
-do.call(text, args=owa(d=list(x=middles,      y=ytitles,  labels=colnames(mat)), argcol))
-do.call(text, args=owa(d=list(x=nameswidth/2, y=ytitles,  labels="tableColVal"), argmain))
-do.call(text, args=owa(d=list(x=nameswidth/2, y=1:nr+0.5, labels=rownames(mat)), argrow))
-# add text to each cell
-do.call(text, args=owa(d=list(x=rep(middles, each=nr), y=rep(1:nr, nc)+0.5, 
-                              labels=as.vector(mat)), argcell))
-# Set old paramaters again:
-par(op)
-if(pdf) { dev.off() ; message("PDF-File is located here:", pdffile, "\n") }# close pdf device
+text(x=xm[-1], y=ym[1] , labels=colnames(mat), ...)
+text(x=xm[1] , y=ym[1] , labels=main,          ...)
+text(x=xm[1] , y=ym[-1], labels=rownames(mat), ...)
+# output
+return(invisible(list(x1=x1,x2=x2,xm=xm,y1=y1,y2=y2,ym=ym)))
 } # end of function
