340313966e887e8f02f93820aa1e31d48eb0f5bc
brry <berryboessenkool@hotmail.com>
2016-12-01 01:59:56 +0100

    tryStack: now returns level numbers and final error provoking code. More tests added.

--- R/tryStack.R
+++ R/tryStack.R
@@ -21,32 +21,39 @@
 #' lower <- function(a) a+10
 #' middle <- function(b) {plot(b, main=b) ; lower(b) }
 #' upper <- function(c) {cat("printing c:", c, "\n") ; middle(c)}
-#' d <- upper(4)
+#' d <- upper(42)
 #' d
 #' rm(d)
 #' 
-#' is.error( d <- upper("4") , tell=TRUE)      # error, no d creation 
+#' is.error( d <- upper("42") , tell=TRUE)      # error, no d creation 
 #' 
-#' d <- try(upper("4"))
+#' d <- try(upper("42"))
 #' d
 #' inherits(d, "try-error")
 #' cat(d)
 #' 
-#' d <- tryStack(upper("4"))
+#' d <- tryStack(upper("42"))
 #' d
 #' inherits(d, "try-error")
 #' cat(d)
-#' cat(tryStack(upper("4")))
+#' cat(tryStack(upper("42")))
+#' 
+#' stopifnot(tryStack(upper(42))==52)
+#' 
+#' 
+#' myfun <- function(k) tryStack(upper("42"))
+#' d <- myfun(42)
+#' cat(d)
 #' 
 #' myfun <- function(k) tryStack(stop("oh oh"))
-#' d <- myfun(4)
-#' cat(d)
+#' d <- myfun(42)
+#' 
 #' d <- tryStack(myfun(4) ) # nested calls get weird
 #' cat(d)                   # They make that empty in the second run, for example.
 #'
-#' myfun <- function(k) cat( tryStack(stop("oh oh")) )
-#' myfun(4) 
-#' d <- tryStack(myfun(4) ) # or give the complete try call...
+#' myfun <- function(k) cat( tryStack(upper("42")) )
+#' myfun(42) 
+#' d <- tryStack(myfun(42) ) # or give the complete try call...
 #'
 #' @param expr Expresssion to try, potentially wrapped in curly braces if spanning several commands.
 #' @param silent Logical: Should error message printing be suppressed?
@@ -75,11 +82,17 @@ out <- try(withCallingHandlers(expr, error=function(e)
   toremove <- "withCallingHandlers(expr, error = function(e) {    stack <- sys.calls()"
   removetoo <- sapply(stack, function(x) grepl(toremove, paste(x[1:2],collapse=""), fixed=TRUE))
   stack <- stack[!removetoo]
+  # add error code:
+  stack <- c(stack, deparse(conditionCall(e))[1L])
+  # add numbers:
+  stack <- sapply(seq_along(stack), function(i) paste0(i, ": ", stack[[i]]))
+  # add descriptor:
+  stack <- c("tryStack sys.calls() error stack: ", stack)
   # put message into main function environment:
   assign("stackmsg", value=paste(stack,collapse="\n"), envir=tryenv)
   # print if not silent:
   if(!silent && isTRUE(getOption("show.error.messages"))) 
-    cat("This is the error stack: ", tryenv$stackmsg, sep="\n")
+    cat(tryenv$stackmsg, sep="\n")
   }), silent=silent)
 # add the trace stack character string to the output:
 if(inherits(out, "try-error")) out[2] <- tryenv$stackmsg
