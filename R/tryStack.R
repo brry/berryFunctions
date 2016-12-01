cde147e6d9217bfb6e0f5ea2c573617e4ae6e9d7
brry <berryboessenkool@hotmail.com>
2016-12-01 01:38:34 +0100

    trystack: can now be inside a function

--- R/tryStack.R
+++ R/tryStack.R
@@ -8,7 +8,12 @@
 #' @return Value of \code{expr} if evaluated successfully. If not, an invisible 
 #' object of class "try-error" as in \code{\link{try}} with the stack in \code{object[2]}.
 #' @author Berry Boessenkool, \email{berry-b@@gmx.de}, Nov 2016
-#' @seealso \code{\link{try}}, \code{\link{traceCall}}
+#' @seealso \code{\link{try}}, \code{\link{traceCall}},
+#' \url{http://r.789695.n4.nabble.com/Stack-trace-td4021537.html},
+#' \url{http://stackoverflow.com/questions/15282471/get-stack-trace-on-trycatched-error-in-r},
+#' \url{http://stackoverflow.com/questions/1975110/printing-stack-trace-and-continuing-after-error-occurs-in-r},
+#' \url{http://stackoverflow.com/questions/16879821/save-traceback-on-error-using-trycatch}
+#' 
 #' @keywords programming error
 #' @export
 #' @examples
@@ -31,7 +36,17 @@
 #' d
 #' inherits(d, "try-error")
 #' cat(d)
+#' cat(tryStack(upper("4")))
+#' 
+#' myfun <- function(k) tryStack(stop("oh oh"))
+#' d <- myfun(4)
+#' cat(d)
+#' d <- tryStack(myfun(4) ) # nested calls get weird
+#' cat(d)                   # They make that empty in the second run, for example.
 #'
+#' myfun <- function(k) cat( tryStack(stop("oh oh")) )
+#' myfun(4) 
+#' d <- tryStack(myfun(4) ) # or give the complete try call...
 #'
 #' @param expr Expresssion to try, potentially wrapped in curly braces if spanning several commands.
 #' @param silent Logical: Should error message printing be suppressed?
@@ -43,22 +58,28 @@ silent=FALSE
 {
 # environment for stack to (potentially) be written into:
 tryenv <- new.env()
+assign("stackmsg", value="-- empty stack --", envir=tryenv)
 # now try the expression:
 out <- try(withCallingHandlers(expr, error=function(e)
   {
   # stack of calls, in case of an error:
   stack <- sys.calls()
+ # browser()
   # remove the tryCatch part:
   stack <- stack[-(2:7)]
   # remove the current part:
   stack <- head(stack, -2)
   # language to character:
   stack <- sapply(stack, deparse)
+  # remove element from tryStack being in a function:
+  toremove <- "withCallingHandlers(expr, error = function(e) {    stack <- sys.calls()"
+  removetoo <- sapply(stack, function(x) grepl(toremove, paste(x[1:2],collapse=""), fixed=TRUE))
+  stack <- stack[!removetoo]
+  # put message into main function environment:
+  assign("stackmsg", value=paste(stack,collapse="\n"), envir=tryenv)
   # print if not silent:
   if(!silent && isTRUE(getOption("show.error.messages"))) 
-    cat("This is the error stack: ", stack, sep="\n")
-  # put message into main function:
-  assign("stackmsg", value=paste(stack,collapse="\n"), envir=tryenv)
+    cat("This is the error stack: ", tryenv$stackmsg, sep="\n")
   }), silent=silent)
 # add the trace stack character string to the output:
 if(inherits(out, "try-error")) out[2] <- tryenv$stackmsg
