c73de1de39d2b366ac7fa72d78f1f9d63368ca1b
brry <berryboessenkool@hotmail.com>
2016-12-05 13:33:29 +0100

    textField doc: link added, closes #2

--- R/textField.R
+++ R/textField.R
@@ -16,8 +16,11 @@
 #' 
 #' @return None
 #' @author Berry Boessenkool, \email{berry-b@@gmx.de}, April 2013 + March 2014
-#' @seealso \code{\link{text}}; \code{shadowtext} in package \code{TeachingDemos}, see \url{http://stackoverflow.com/questions/25631216};
-#'          \code{s.label} in package \code{ade4}, which is not so versatile and doesn't work with logarithmic axes
+#' @seealso \code{\link{text}}; \code{shadowtext} in package \code{TeachingDemos}, see
+#'          \url{http://blog.revolutionanalytics.com/2009/05/make-text-stand-out-with-outlines.html}, 
+#'           and \url{http://stackoverflow.com/questions/25631216}. \cr
+#'          \code{s.label} in package \code{ade4}, which is not so versatile and 
+#'          doesn't work with logarithmic axes
 #' @references with inspiration taken from \code{ordilabel} in package \code{vegan} and thanks to Jari Oksanen for his comments
 #' @keywords aplot
 #' @importFrom graphics par polygon rect strheight text
