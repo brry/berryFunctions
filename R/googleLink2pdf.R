1523a7fa12e0fb36ca122fe5818cb49638d7e903
brry <berryboessenkool@hotmail.com>
2017-01-06 12:24:09 +0100

    googleLink2pdf: all regular hex characters converted to ascii

--- R/googleLink2pdf.R
+++ R/googleLink2pdf.R
@@ -16,6 +16,17 @@
 #'         "&usg=AFQjCNGejDwPlor4togQZmQEQv72cK9z8A&bvm=bv.45580626,d.Yms")
 #' googleLink2pdf(Link)
 #' 
+#' Link <- paste0("https://www.google.de/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1",
+#'         "&cad=rja&uact=8&ved=0ahUKEwjLlfmClavRAhWaN1AKHcGSBjEQFgghMAA",
+#'         "&url=http%3A%2F%2Fstackoverflow.com%2Fquestions%2Ftagged%2Fr",
+#'         "&usg=AFQjCNHYj6HjSs6Lvczn9wMWxE3slCdq1Q&bvm=bv.142059868,d.ZWM")
+#' googleLink2pdf(Link)
+#' Link <- paste0("https://www.google.de/url?sa=t&rct=j&q=&esrc=s&source=web&cd=2",
+#'         "&cad=rja&uact=8&ved=0ahUKEwjLlfmClavRAhWaN1AKHcGSBjEQFggpMAE&",
+#'         "url=http%3A%2F%2Fstackoverflow.com%2Fquestions%2Ftagged%2F%3Ftagnames",
+#'         "%3Dr%26sort%3Dactive&usg=AFQjCNGkPGHq05qwKLLW4vRXdmk2Olhmig&bvm=bv.142059868,d.ZWM")
+#' googleLink2pdf(Link)
+#' 
 #' @param googlelink Character string: A search result address
 #' 
 googleLink2pdf <- function(
@@ -25,9 +36,11 @@ googlelink
 pdflink <- strsplit(googlelink, "&url=")[[1]][2]
 pdflink <- strsplit(pdflink, "&ei=")[[1]][1]
 pdflink <- strsplit(pdflink, "&usg=")[[1]][1]
-pdflink <- gsub("%24", "$", pdflink)
-pdflink <- gsub("%2F", "/", pdflink)
-pdflink <- gsub("%3A", ":", pdflink)
-pdflink <- gsub("%25", "%", pdflink)
+# printable ascii characters from http://www.ascii-code.com/
+hex <- "20 21 22 23 24 25 26 27 28 29 2A 2B 2C 2D 2F 3A 3B 3C 3D 3E 3F 40"
+ascii <- "! \" # $ % & ' ( ) * + , - / : ; < = > ? @"
+hex <- paste0("%",strsplit(hex, " ")[[1]])
+ascii <- c(" ",strsplit(ascii, " ")[[1]])
+for(i in seq_along(hex)) pdflink <- gsub(hex[i], ascii[i], pdflink)
 pdflink
 }
