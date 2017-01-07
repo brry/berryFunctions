541f4d947ac6c75f3cd2d39369d9ba4a9be5224a
brry <berryboessenkool@hotmail.com>
2017-01-07 11:36:05 +0100

    l2array: now uses abind for much faster results

--- R/l2array.R
+++ R/l2array.R
@@ -1,13 +1,36 @@
 #' Convert list of arrays to array
 #'
-#' Convert a list of arrays to a single array, conserving names
+#' Convert a list of arrays to a single array, conserving names.
+#' You can also directly use
+#' \code{do.call(abind::abind, list(LIST, rev.along=0, use.dnns=TRUE)) }
 #'
 #' @return array
 #' @author Berry Boessenkool, \email{berry-b@@gmx.de}, Dec 2016
-#' @seealso \code{\link{l2df}}, \code{\link{help}}
+#' @seealso \code{\link{l2df}}, \code{\link{help}}, \url{http://stackoverflow.com/a/4310747}
 #' @keywords list manip array
+#' @importFrom abind abind
 #' @export
 #' @examples
+#' 
+#' LIST <- lapply(LETTERS[1:5], function(x) array(paste0(x,1:24), dim=c(3,4,2)))
+#' str(LIST)
+#' LIST[[2]]
+#' LISTa1 <- l2array(LIST)
+#' LISTa1
+#' str(LISTa1)
+#' 
+#' # The old l2array (<1.13.14, 2017-01-06) was very slow on large lists.
+#' # I then found abind, which is much much much faster and easier on memory!
+#' # It now replaces the internal old actual conversion code
+#' # l2array still checks the dimnames
+#' LISTa2 <- do.call(abind::abind, list(LIST, rev.along=0, use.dnns=TRUE)) 
+#' LISTa2
+#' stopifnot(all(LISTa1==LISTa2))
+#' rm(LIST, LISTa1, LISTa2)
+#' 
+#'  
+#' # General intro to arrays -----
+#' 
 #' A1 <- array(1:24, dim=c(4,2,3), dimnames=list(
 #'                    my_x=paste0("row",1:4), my_y=c("A","B"), paste0("n",1:3)))
 #' A1
@@ -16,7 +39,7 @@
 #' # Selection:
 #' A1[,,"n2"]
 #' A1[,,1:2]
-#' A1["row2",,] # result rotated against expectation
+#' A1["row2",,] # result rotated against expectation -> transpose with t(...)
 #' A1[,"A",]
 #' # aggregation:
 #' apply(A1, MARGIN=1:2, FUN=sum) # keep first two dimensions
@@ -24,50 +47,53 @@
 #' A1["row1",,]                                    # 1+5=6, 9+13=22, 17+21=38
 #' 
 #' as.vector(A1)
+#' 
+#' A <- array(1:24, dim=c(3,4,2), dimnames=list(x=paste0("x",1:3), 
+#'                                              y=paste0("y",1:4), 
+#'                                              z=paste0("z",1:2)))
+#' str(A)
+#' rm(A)
+#' 
+#' 
+#' # l2array -----
+#' 
 #' A2 <- A1+2
 #' A3 <- A1+4
+#' LIST <- list(A1=A1, A2=A2, A3=A3) # list of arrays
 #' 
-#' LA <- list(A1=A1, A2=A2, A3=A3) # list of arrays
-#' 
-#' AA <- l2array(LA)
-#' AA
-#' AA[,,,"A2"]
-#' avg <- apply(AA, MARGIN=1:3, mean)
+#' LA <- l2array(LIST)
+#' LA
+#' str(LA)
+#' LA[,,,"A2"]
+#' LA["row2", ,"n2",]
+#' avg <- apply(LA, MARGIN=1:3, mean)
 #' stopifnot(all(avg==A2))
 #' 
-#' # names check:
-#' LN <- LA
-#' names(dimnames(LN[[2]]))[3] <- "intentional"
-#' dimnames(LN[[2]])[3] <- list(paste0("k",1:3))
-#' AN <- l2array(LN)
 #' 
-#' LN <- LA
-#' rownames(LN[[3]])[2] <- "intentional_diff"
-#' A <- l2array(LN)
+#' # names check -----
+#' 
+#' LISTN <- LIST
+#' names(dimnames(LISTN[[2]]))[3] <- "intentional"
+#' dimnames(LISTN[[3]])[3] <- list(paste0("k",1:3))
+#' LAN <- l2array(LISTN)
+#' LAN["row2", ,"k2",] # n2 is now changed to k2
+#' LANa <- do.call(abind::abind, list(LISTN, rev.along=0, use.dnns=TRUE)) 
+#' all(LAN==LANa)
+#' str(LANa)
+#' 
+#' LISTN <- LIST
+#' rownames(LISTN[[3]])[2] <- "intentional_diff"
+#' LAN <- l2array(LISTN)
 #' 
 #' # data type check
-#' A <- l2array(c(LA, 999))
+#' is.error(   A <- l2array(c(LA, 999)),  tell=TRUE, force=TRUE)
 #' 
 #' 
-#' A <- array(1:24, dim=c(2,3,4), dimnames=list(x=paste0("x",1:2), 
-#'                                              y=paste0("y",1:3), 
-#'                                              z=paste0("z",1:4)))
-#' str(A)
-#' 
-#' 
-#' 
-#'
 #' @param x List with arrays. The dimension of the first is target dimension.
-#' @param usenames   Logical: Should names be kept? The names of the first element 
-#'                   will be used. DEFAULT: TRUE
-#' @param checknames Logical: Should names of other elements be the same as in x[[1]]?
-#'                   Will be checked with useful warning messages. DEFAULT: TRUE
-#'
+#' @param \dots Further arguments passed to \code{abind::\link{abind}}
 l2array <- function(
 x,
-usenames=TRUE,
-checknames=TRUE
-)
+...)
 {
 # input checks:
 if(!is.list(x)) stop("x must be a list, not a ", class(x))
@@ -80,14 +106,14 @@ if(!all(isar)) warning("all elements in x should be arrays. The following are no
 dina <- dimnames(x[[1]]) 
 elna <- names(x)
 # check names:
-if(checknames && length(x)>1)
+if(length(x)>1)
   {
   ###message("Checking dimnames names..."); flush.console()
   # check names of dimnames:
   dummy <- lapply(2:length(x), function(i){
   dina2 <- dimnames(x[[i]])
   ncomp <- names(dina) == names(dina2)
-  if(!all(ncomp)) warning("in l2array: dimnames are not equal to output names in x[[", i, "]]:\n  '",
+  if(!all(ncomp)) warning("in l2array: names of dimnames are not equal to output names in x[[", i, "]]:\n  '",
                        toString(names(dina2)), "' instead of '", toString(names(dina)), "'.", call.=FALSE)
   })
   # check array dimnames:
@@ -100,9 +126,7 @@ if(checknames && length(x)>1)
                        toString(dina2[[j]]), "' instead of '", toString(dina[[j]]), "'.", call.=FALSE)
   })})
   }
-outnames <- NULL
-if(usenames) outnames <- c(dina, list(elna))
-###message("transforming to array..."); flush.console()
-out <- array(unlist(x), dim=c(dim(x[[1]]),length(x)), dimnames=outnames)
+# actually convert to array:
+out <- do.call(abind::abind, list(x, rev.along=0, use.dnns=TRUE, ...))
 out
 }
