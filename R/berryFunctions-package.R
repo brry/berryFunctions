70b45dadf74c1647ba4a53a676dede8caed7b617
brry <berryboessenkool@hotmail.com>
2017-01-25 13:34:31 +0100

    dataDWD and readDWD removed (now in package rdwd)

--- R/berryFunctions-package.R
+++ R/berryFunctions-package.R
@@ -34,3 +34,14 @@
 #' 
 NULL
 
+
+#' Get climate data from the German Weather Service (DWD) FTP-server.
+#' 
+#' \code{dataDWD} and \code{readDWD} have been greatly improved and are now in the package \code{rdwd}.
+#' This provides a much better infrastructure for file selection and downloading. 
+#' See \url{https://github.com/brry/rdwd#rdwd} for usage instructions.
+#' 
+#' @name dataDWD
+#' @aliases dataDWD readDWD
+#' 
+NULL
