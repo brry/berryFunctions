4fe3a032239868c0c9b604a40f551e65de7ece6b
brry <berryboessenkool@hotmail.com>
2017-02-21 13:24:03 +0100

    newFilename: code indented, message improved

--- R/newFilename.R
+++ R/newFilename.R
@@ -20,6 +20,7 @@
 #' newFilename(fns, ignore=rep(0:1, each=4))
 #' newFilename(fns, ntrunc=2)
 #' newFilename("README.md")
+#' newFilename("dummy", mid="") # no line break
 #'
 #' @param filename Char (vector): file name(s).
 #' @param ignore   Logical (vector, recycled): Ignore file? DEFAULT: FALSE
@@ -41,23 +42,23 @@ ntrunc=3
 {
 ignore <- rep(ignore, length.out=length(filename))
 output <- lapply(seq_along(filename), function(i)
-{
-f <- filename[i]
-if(ignore[i]) return(c(NA, f))
-#
-e2 <- tools::file_ext(f)
-if(e2!="") e2 <- paste0(".",e2)
-e1 <- tools::file_path_sans_ext(f)
-existed <- FALSE
-nr <- 1
-while(file.exists(f))
-{
-  f <- paste0(e1,"_",nr,e2)
-  nr <- nr + 1
-  existed <- TRUE
-}
-return(c(existed, f))
-})
+  {
+  f <- filename[i]
+  if(ignore[i]) return(c(NA, f))
+  #
+  e2 <- tools::file_ext(f)
+  if(e2!="") e2 <- paste0(".",e2)
+  e1 <- tools::file_path_sans_ext(f)
+  existed <- FALSE
+  nr <- 1
+  while(file.exists(f))
+    {
+    f <- paste0(e1,"_",nr,e2)
+    nr <- nr + 1
+    existed <- TRUE
+    }
+  return(c(existed, f))
+  })
 fnames  <- sapply(output, "[", 2)
 existed <- sapply(output, "[", 1)
 existed <- as.logical(existed)
@@ -67,9 +68,12 @@ if(!quiet)
   n_e <- sum(existed, na.rm=TRUE) # number of existing files
   n_n <- sum(!is.na(existed)) # number of new files
   n_i <- sum(is.na(existed)) # number of ignored files
+  n_o <- sum(file.exists(fnames[is.na(existed)])) # overwritten
+  n_i <- n_i-n_o
   message(traceCall(1, "", ": "), pre,
-          if(n_i>0) paste0("ignoring ", n_i, " file", if(n_i>1) "s", "; "),
-          if(n_n>0) paste0("creating ", n_n, " file", if(n_n>1) "s"),
+          if(n_i>0) paste0(   "ignoring ", n_i, " file", if(n_i>1)"s", if(n_o>0|n_n>0)", "),
+          if(n_o>0) paste0("overwriting ", n_o, " file", if(n_o>1)"s", if(n_n>0)", "),
+          if(n_n>0) paste0(   "creating ", n_n, " file", if(n_n>1)"s"),
           if(n_e>0) paste0(" (",n_e," already existed for which '_n' is appended)"),
           ":", mid, truncMessage(fnames, ntrunc=ntrunc, prefix=""), end)
   }
