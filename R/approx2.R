36887f7bb2340ec2196a6f25b6f6d2630db2b59b
brry <berryboessenkool@hotmail.com>
2017-08-09 17:02:41 +0200

    approx2: exampes improved

--- R/approx2.R
+++ R/approx2.R
@@ -17,13 +17,13 @@
 #' @export
 #' @examples
 #' 
-#' is.error(  approx2(c(NA,NA))  ) # yields an error
-#' approx2(c(NA,NA, 6, 4, 8, 9, 3, 2, 1))
-#' approx2(c( 2,NA, 6, 4, 8, 9, 3, 2, 1))
-#' approx2(c( 2, 4, 6, 4, 8, 9,NA, 2,NA))
+#' approx2(c(NA,NA))   # yields a message
+#' approx2(c(NA,NA, 6, 4, 8, 9, 3, 2, 1)) # fills with first non-NA value
+#' approx2(c( 2,NA, 6, 4, 8, 9, 3, 2, 1)) # interpolates linearly
+#' approx2(c( 2, 4, 6, 4, 8, 9,NA, 2,NA)) # linear, then last non-NA at end
 #' 
 #' approx2(c(NA,NA, 6, 4, 8, 9, 3, 2, 1))
-#' approx2(c(NA,NA, 6, 4, 8, 9, 3, 2, 1), fill=median)
+#' approx2(c(NA,NA, 6, 4, 8, 9, 3, 2, 1), fill=median) # first median, then linear
 #' approx2(c(NA,NA, 6, 4, 8, 9, 3, 2, 1), fill=mean)
 #' 
 #' approx2(c( 3, 4, 6, 4, 8, 9,NA, 2,NA))
