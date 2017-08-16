1fc32710aa49b06c6c7ad712331a4ab9abc5b9b5
brry <berryboessenkool@hotmail.com>
2017-08-16 14:22:49 +0200

    round0: 'pre' argument explanation improved

--- R/round0.R
+++ R/round0.R
@@ -10,13 +10,13 @@
 #' round0( pi*10^(-3:5), 2)
 #' stopifnot(round0(17.3, 2) == "17.30")
 #' round0(7.3)
-#' round0(7.3, 2)
+#' round0(c(7.3,777.1234), 2)
 #' round0(c(0.2,7.3,12.8), 2, pre=1)
 #' round0(c(0.2,7.3,12.8), 1, pre=3, flag="") # spaces instead of zeros
 #' 
 #' @param x      Value(s)
 #' @param digits Number of digits (after decimal separator) to keep. DEFAULT: 0
-#' @param pre    Number of characters before the decimal separator. DEFAULT: 2
+#' @param pre    Minimum number of characters before the decimal separator. DEFAULT: 2
 #' @param width  Total width (number of characters including dot).
 #'               DEFAULT: digits+pre (+1 if needed)
 #' @param flag   Flag. Could be "" for spaces. DEFAULT: "0"
