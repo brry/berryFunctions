50c70576bafcc5ca6cc35ac13279ac12e23f8901
brry <berryboessenkool@hotmail.com>
2017-12-16 20:56:21 +0100

    rectround: gains option to select corners, coordinates more readable, coordinates returned invisibly as output

--- R/rectround.R
+++ R/rectround.R
@@ -1,10 +1,9 @@
 #' @title Rectangles with rounded corners
 #' @description Draw \link{rect}angles with rounded corners via \code{\link{polygon}}
-#' @return ReturnValue
+#' @return Final coordinates, invisible
 #' @author Berry Boessenkool, \email{berry-b@@gmx.de}, Dec 2017
 #' @seealso \code{\link{textField}}
 #' @keywords aplot
-# @importFrom package fun1 fun2
 #' @export
 #' @examples
 #' plot(1:10)  ;  rect(4,2,7,8, border=8)
@@ -13,6 +12,9 @@
 #' rectround(4,2,7,8, rounding=0.5)
 #' rectround(4,2,7,8, rounding=-0.1, border="red")
 #' rectround(4,2,7,8, rounding=0.8, border="blue")
+#' 
+#' plot(1:10)  ;  rect(4,2,7,8, border=8)
+#' rectround(4,2,7,8, corners=c(2,4))
 #' 
 #' plot(1:10, asp=1)  ;  rect(4,2,7,8, border=8)
 #' rectround(4,2,7,8)
@@ -36,6 +38,8 @@
 #'                    both x and y direction? If TRUE,
 #'                    the proportion relates to the shortest rectangle side.
 #'                    DEFAULT: FALSE
+#' @param corners     Vector with integers indicating the corners to round.
+#'                    Starting bottom left, going clockwise. DEFAULT: 1:4
 #' @param \dots       Further arguments passed to \code{\link{polygon}},
 #'                    like col, border, ...
 #' 
@@ -44,6 +48,7 @@ xleft, ybottom, xright, ytop,
 rounding=0.25,
 aspcorrect=FALSE,
 bothsame=FALSE,
+corners=1:4,
 ...)
 {
 # abbreviated inputs and checks:
@@ -59,7 +64,9 @@ if(length(YT)>1) stop(    "ytop must be a single value, not ", length(YT))
 if(length(RR)>1) stop("rounding must be a single value, not ", length(RR))
 if(RR>1) warning("rounding recommended to be smaller than 1. It is ", RR)
 if(RR<0) warning("rounding recommended to be larger than 0. It is ", RR)
-
+if(!is.numeric(corners)) stop("corners must be ingtegers, not ", class(corners))
+if(!all(corners %in% 1:4)) stop("corners must be (some of) the integers 1:4, not ", 
+                                toString(corners))
 asp <- diff(par("usr")[3:4])/diff(par("usr")[1:2]) # current aspect ratio y to x
 
 XD <- XR-XL
@@ -76,9 +83,14 @@ elx <- function(from,to) xi*cos(seq(from,to,length.out=nv/4))
 ely <- function(from,to) yi*sin(seq(from,to,length.out=nv/4))
 
 # x and y coordinates:
-xc <- c(XR-xi+elx(0,pi/2), XR-xi, XL+xi, XL+xi+elx(pi/2,pi), XL,    XL,
-        XL+xi+elx(pi,3*pi/2), XL+xi, XR-xi, XR-xi+elx(3*pi/2,2*pi), XR,    XR)
-yc <- c(YT-yi+ely(0,pi/2), YT,    YT,    YT-yi+ely(pi/2,pi), YT-yi, YB+yi,
-        YB+yi+ely(pi,3*pi/2), YB,    YB,    YB+yi+ely(3*pi/2,2*pi), YB+yi, YT-yi)
+xc <- c(if(3 %in% corners) XR-xi+elx(0     ,pi/2  ) else XR, # corner 3 TR
+        if(2 %in% corners) XL+xi+elx(pi/2  ,pi    ) else XL, # corner 2 TL
+        if(1 %in% corners) XL+xi+elx(pi    ,3*pi/2) else XL, # corner 1 BL
+        if(4 %in% corners) XR-xi+elx(3*pi/2,2*pi  ) else XR) # corner 4 BR
+yc <- c(if(3 %in% corners) YT-yi+ely(0     ,pi/2  ) else YT, # corner 3 TR
+        if(2 %in% corners) YT-yi+ely(pi/2  ,pi    ) else YT, # corner 2 TL
+        if(1 %in% corners) YB+yi+ely(pi    ,3*pi/2) else YB, # corner 1 BL
+        if(4 %in% corners) YB+yi+ely(3*pi/2,2*pi  ) else YB) # corner 4 BR
 polygon(x=xc, y=yc, ...)
+invisible(data.frame(x=xc,y=yc))
 }
