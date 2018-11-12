e03b5cc21c3a7277027202d2160fe5f8eb95dd0d
brry <berryboessenkool@hotmail.com>
2018-11-12 07:29:48 +0100

    dataStr: returns result for further inspection, added example for selection of class data.frame

--- R/dataStr.R
+++ R/dataStr.R
@@ -2,7 +2,7 @@
 #' 
 #' Print the \code{\link{str}} of each dataset returned by \code{\link{data}}
 #' 
-#' @return NULL. prints via \code{\link{message}} in a for loop.
+#' @return invisible data.frame. Mainly prints via \code{\link{message}} in a for loop.
 #' @author Berry Boessenkool, \email{berry-b@@gmx.de}, November 2015, in search of good datasets for teaching
 #' @seealso \code{\link{str}}
 #' @keywords print documentation
@@ -13,6 +13,23 @@
 #' dataStr() # all loaded packages on search path (package=NULL)
 #' dataStr("datasets") # only datasets in base R
 #' dataStr("colorspace") # works with an installed but unloaded package
+#' 
+#' 
+#' # Selection of dataframes based on columns ----
+#' d <- dataStr()
+#' d <- d[grepl("data.frame", d$class), ]
+#' d$nrow <- NA
+#' d$ncol <- NA
+#' env <- new.env()
+#' for(i in 1:nrow(d))
+#'   {
+#'   x <- d[i,, drop=FALSE]
+#'   data(list=x$Call, package=x$Package, envir=env)
+#'   obj <- get(x$Object, envir=env) # getExportedValue(asNamespace(package), x$Object)
+#'   d[i, c("nrow","ncol")] <- c(nrow(obj),ncol(obj))
+#'   }
+#' d <- sortDF(d, ncol)
+#' d[,c("Call","ncol","nrow")]
 #' 
 #' @param package Package name. DEFAULT: NULL
 #' @param \dots other arguments passed to \code{\link{data}}
@@ -33,6 +50,7 @@ d$Call <- gsub("(","",gsub(")","",d$Call, fixed=TRUE), fixed=TRUE)
 d$Call[is.na(d$Call)] <- d$Object[is.na(d$Call)]
 # sort alphabetically within packages:
 d <- d[order(d$Package, tolower(d$Object)),]
+d$class <- NA
 for(i in 1:nrow(d))
   {
   x <- d[i,, drop=FALSE]
@@ -40,7 +58,9 @@ for(i in 1:nrow(d))
   obj <- get(x$Object, envir=env) # getExportedValue(asNamespace(package), x$Object)
   message(x$Package, "  |  ", x$Object, "  |  ", toString(class(obj)), "  |  ", x$Title)
   message(str(obj))
+  d[i,"class"] <- toString(class(obj))
   }
+return(invisible(d))
 }
 
 
