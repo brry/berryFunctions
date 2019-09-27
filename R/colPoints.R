9d700c2035b3fd75c5a7dc7fc5a436826d198be6
brry <berryboessenkool@hotmail.com>
2019-09-27 12:16:40 +0200

    colPoints: dimension message with * instead of ,

--- R/colPoints.R
+++ R/colPoints.R
@@ -225,8 +225,8 @@ if(is.vector(z))
    if(missing(x)) {x <- 1:ncol(z) ; if(missing(xlab)) xlab <- "x" }
    if(missing(y)) {y <- 1:nrow(z) ; if(missing(ylab)) ylab <- "y" }
    if(!(length(x)==ncol(z) & length(y)==nrow(z)))
-     stop("Dimension of z (ncol*nrow=",ncol(z),",",nrow(z),
-          ") is not length(x) * length(y) (=",length(x),",",length(y),")!")
+     stop("Dimension of z (ncol*nrow=",ncol(z),"*",nrow(z),
+          ") is not length(x) * length(y) (=",length(x),"*",length(y),")!")
    }
 # error checking:
 if(diff(range(z, finite=TRUE))==0) if(!quiet) warning("All z-values are equal.")
