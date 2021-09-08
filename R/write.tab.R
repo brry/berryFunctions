9bd5372143200fb0b470de6b3eec25f71b7e33cd
brry <berryboessenkool@hotmail.com>
2021-09-08 12:56:47 -0400

    write.tab: gains argument 'name_rn' to easily prepend the rownames in a proper column

--- R/write.tab.R
+++ R/write.tab.R
@@ -12,15 +12,21 @@
 #' \dontrun{
 #' write.tab(iris)
 #' write.tab(iris, "otherfile.txt")
+#' write.tab(freeny)
+#' write.tab(freeny, name_rn="Time")
 #' unlink("iris.txt")
 #' unlink("otherfile.txt")
+#' unlink("freeny.txt")
 #' }
 #' @param x            Objekt to be written.
 #' @param file         Filename. DEFAULT: NULL = [name of x].txt
 #' @param quote        Write quatation marks around charstrings? DEFAULT: FALSE
 #' @param sep          Column separator. DEFAULT: "\t"
+#' @param name_rn      If not NULL, this will be used as the name for a prepended
+#'                     column with the rownames. DEFAULT: NULL
 #' @param row.names    Should rownames be written in a pre-column that will
-#'                     mess up alignment with column mnames? DEFAULT: FALSE
+#'                     mess up alignment with column mnames? 
+#'                     Use name_rn instead. DEFAULT: FALSE
 #' @param fileEncoding Encoding of charstrings. DEFAULT: "UTF-8"
 #' @param open         Try to open the output file? DEFAULT: TRUE
 #' @param \dots        Further arguments passed to \code{\link{write.table}}
@@ -29,6 +35,7 @@ write.tab <- function(
 x,
 file=NULL,
 sep="\t",
+name_rn=NULL,
 row.names=FALSE,
 quote=FALSE,
 fileEncoding="UTF-8",
@@ -37,6 +44,7 @@ open=TRUE,
 )
 {
 if(is.null(file)) file <- paste0(deparse(substitute(x)), ".txt")
+if(!is.null(name_rn)) {x <- cbind(rownames(x), x) ; colnames(x)[1] <- name_rn}
 write.table(x=x, file=file, sep=sep, row.names=row.names, quote=quote, fileEncoding=fileEncoding)
 if(open) openFile(file)
 return(normalizePath(file))
