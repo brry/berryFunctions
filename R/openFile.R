077c903b1d56d7a379051ca4320b889c29b5965d
brry <berryboessenkool@hotmail.com>
2025-10-21 00:49:11 +0200

    openFile fixed on Windows for sumatraPDF

--- R/openFile.R
+++ R/openFile.R
@@ -39,8 +39,8 @@ sysname <- Sys.info()["sysname"]
 out <- try(switch(sysname,
                   "Linux"   = system2("xdg-open", file, ...),
                   "FreeBSD" = system2("handlr", paste("open",file), ...),
-                  # system2("open", file, ...)  # closes sumatra when switching Rstudio projects
-                  for(fn in file) system2("cmd", args=c("/c","start","/B",'""',fn), wait=FALSE)
+                  "Windows" = for(fn in file) system2("cmd", args=c("/c","start","/B",'""',fn), wait=FALSE),
+                  system2("open", file, ...) # on Windows, this closes sumatra when switching Rstudio projects
                   # system2("powershell", args=c("-Command", paste0("Start-Process '",file,"'")), wait=FALSE)
                   ),
            silent=TRUE)
